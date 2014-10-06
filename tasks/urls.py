bbc9df99e17ea93786dd22195d333fc52fc7dd49
spoons <dominik.muhs@mailbox.tu-dresden.de>
2014-10-06 21:31:28 +0200

    Cleaned url files of useless comments

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,9 +3,6 @@ from django.conf.urls import patterns, url
 from tasks import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'prktmt.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.index, name='index'),
     url(r'^task/(?P<slug>[-\w]+)/$', views.detail, name='detail'),
     url(r'^task/(?P<slug>\w+)/edit/$', views.edit, name='edit'),
