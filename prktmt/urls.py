08e40ee71bd22369ed61afa5e7573c2c78b716a5
spoons <dominik.muhs@mailbox.tu-dresden.de>
2014-10-07 23:59:15 +0200

    Added navbar styling and username display after login

--- prktmt/urls.py
+++ prktmt/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+# To serve static files during development!
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     url(r'^tasks/', include('tasks.urls', namespace='tasks')),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
