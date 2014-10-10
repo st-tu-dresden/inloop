af2649a6f21438c5abec565954526fe189faff2a
spoons <dominik.muhs@mailbox.tu-dresden.de>
2014-10-10 18:09:42 +0200

    preparing to merge

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from tinymce.widgets import TinyMCE
 
 class UserEditorForm(forms.Form):
-  content = forms.CharField(label='', widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+  content = forms.CharField(label='', widget=TinyMCE(attrs={'cols': 100, 'rows': 20}))
