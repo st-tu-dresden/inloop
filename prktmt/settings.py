89edd937e55b707a7b0ce3a7c7315c9095d66b40
spoons <dominik.muhs@mailbox.tu-dresden.de>
2015-01-26 20:02:57 +0100

    reconfigured sites and flatpages module, admin can now add flatpages in interface with tinymce, corrected links in base-template, now only the catchall pattern in the base urlconf is active and resolves flatpages

--- prktmt/settings.py
+++ prktmt/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 SITE_ID = 1
