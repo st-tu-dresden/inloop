70540dff8ede2b68120d9798a20e18f4cad2030a
spoons <dominik.muhs@mailbox.tu-dresden.de>
2015-01-26 19:24:10 +0100

    fixed #8 Chrome autocomplete issue in registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,10 +4,18 @@ from accounts.validators import validate_mat_num
 
 
 class UserForm(forms.ModelForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    mat_num = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}), validators=[validate_mat_num])
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'autocomplete': 'off'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'autocomplete': 'off'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'autocomplete': 'off'}))
+    mat_num = forms.IntegerField(widget=forms.NumberInput(attrs={
+        'class': 'form-control',
+        'autocomplete': 'off'}), validators=[validate_mat_num])
 
     class Meta:
         model = UserProfile
