905b1183d0e250b6396d6d6ef0bc082c5b084136
dmuhs <dominik.muhs@mailbox.tu-dresden.de>
2015-03-19 21:52:39 +0100

    fixed slug reverse matching issue

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns(
     url(r'^$', views.index, name='index'),
     url(r'^new_exercise/$', views.submit_new_exercise, name='new_exercise'),
     url(r'^(?P<slug>[-\w]+)/$', views.detail, name='detail'),
-    url(r'^(?P<slug>\w+)/edit/$', views.edit, name='edit'),
-    url(r'^(?P<slug>\w+)/results/$', views.results, name='results'),
-    url(r'^(?P<slug>\w+)/delete/$', views.delete, name='delete'),
+    url(r'^(?P<slug>[-\w]+)/edit/$', views.edit, name='edit'),
+    url(r'^(?P<slug>[-\w]+)/results/$', views.results, name='results'),
+    url(r'^(?P<slug>[-\w]+)/delete/$', views.delete, name='delete'),
 )
