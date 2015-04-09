1822926da741ed90fd993541fca45e54ba97f3be
dmuhs <dominik.muhs@mailbox.tu-dresden.de>
2015-04-09 23:12:03 +0200

    added dynamic task categories - function to add them required

--- tasks/admin.py
+++ tasks/admin.py
@@ -15,7 +15,7 @@ class TaskAdmin(admin.ModelAdmin):
         ('Content', {'fields': ['description', 'slug']})
     ]
     list_display = ('title', 'publication_date', 'deadline_date', 'author')
-    list_filter = ['category', 'publication_date', 'deadline_date']
+    list_filter = ['publication_date', 'deadline_date']
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
 #    inlines = [TaskFileInline]
--- tasks/models.py
+++ tasks/models.py
@@ -9,10 +9,38 @@ TASK_CATEGORIES = (('B', 'Basic Exercise'),
                    ('E', 'Exam Exercise'))
 
 
+def get_task_categories():
+    # TODO: CHECK LOGIC
+    res = ()
+    for c in TaskCategory.objects.all():
+        res += c.get_tuple()
+    return res
+
+
+class TaskCategory(models.Model):
+    short_id = models.CharField(
+        max_length=2,
+        help_text='Short ID for the database')
+    name = models.CharField(
+        max_length=50,
+        help_text='Category Name')
+
+    def get_tuple(self):
+        return (self.short_id, self.name)
+
+
 class Task(models.Model):
     '''
     Represents the tasks that are presented to the user to solve.
     '''
+
+    def __init__(self, *args, **kwargs):
+        super(Task, self).__init__(*args, **kwargs)
+        self.category = models.CharField(
+            max_length=1,
+            choices=get_task_categories,
+            help_text='Task category')
+
     title = models.CharField(max_length=100, help_text='Task name')
     author = models.ForeignKey(UserProfile)
     description = models.TextField(help_text='Task description')
@@ -20,9 +48,10 @@ class Task(models.Model):
         help_text='When should the task be published?'
     )
     deadline_date = models.DateTimeField(help_text='Date the task is due to')
-    category = models.CharField(max_length=1,
-                                choices=TASK_CATEGORIES,
-                                help_text='Category of task')
+    # category = models.CharField(max_length=1,
+    #                            choices=TASK_CATEGORIES,
+    #                            help_text='Category of task')
+    category = models.OneToOneField(TaskCategory)
     slug = models.SlugField(max_length=50, unique=True, help_text='URL name')
 
     def is_active(self):
