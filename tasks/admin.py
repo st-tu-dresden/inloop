f08d721569ee16ff4ea8ff3156899c2bb698e206
dmuhs <dominik.muhs@mailbox.tu-dresden.de>
2015-04-10 01:15:33 +0200

    removed unnecessary stuff

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from tasks.models import Task
 
 
-# class TaskFileInline(admin.TabularInline):
-#     model = TaskSolutionFile
-#     extra = 1
-
-
 class TaskAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['title', 'author', 'category']}),
@@ -18,7 +13,6 @@ class TaskAdmin(admin.ModelAdmin):
     list_filter = ['publication_date', 'deadline_date']
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
-#    inlines = [TaskFileInline]
 
 
 admin.site.register(Task, TaskAdmin)
