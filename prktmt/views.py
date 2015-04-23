8f426de28f327fcfaf1a427d095443805836328c
dmuhs <dominik.muhs@mailbox.tu-dresden.de>
2015-04-23 09:52:00 +0200

    fixed status codes

--- prktmt/views.py
+++ prktmt/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render_to_response
 
 
 def handler404(request):
-    return render_to_response('404.html')
+    response = render_to_response('404.html')
+    response.status_code = 404
+    return response
 
 
 def handler500(request):
-    return render_to_response('500.html')
+    response = render_to_response('500.html')
+    response.status_code = 500
+    return response
