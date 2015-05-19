b76b1944bb4da6570f8c2c767c7e824288b1eb84
dmuhs <dominik.muhs@mailbox.tu-dresden.de>
2015-05-19 15:46:25 +0200

    fixed old migration definition

--- tasks/migrations/0009_auto_20150518_1207.py
+++ tasks/migrations/0009_auto_20150518_1207.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name='TaskSolutionFile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('file', models.FileField(storage=tasks.models.get_storage_system, upload_to=b'')),
+                ('file', models.FileField(upload_to=tasks.models.get_upload_path)),
                 ('solution', models.ForeignKey(to='tasks.TaskSolution')),
             ],
             options={
