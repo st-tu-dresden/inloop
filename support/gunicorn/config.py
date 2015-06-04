d79e41ec1aff0eb46727b9cc4171c8a584f9f1a5
Martin Morgenstern <rm.morgenstern@gmail.com>
2015-06-04 09:44:15 +0200

    Use a config file for gunicorn

---
+++ support/gunicorn/config.py
@@ -0,0 +1,23 @@
+import multiprocessing
+
+# Pidfile is useful for some management tools
+pidfile = '/var/run/inloop/master.pid'
+
+# Listen _only_ on local interface
+bind = "127.0.0.1:8000"
+
+# Baseline, can be adjusted at runtime using TTIN/TTOU
+# signals to the gunicorn master
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = 1
+
+# Rejuvenation to prevent memory leaks
+max_requests = 5000
+max_requests_jitter = 500
+
+# Access logging is done by the reverse proxy (= nginx)
+accesslog = None
+
+# Error logging
+errorlog = '/var/log/inloop/error.log'
+loglevel = 'info'
