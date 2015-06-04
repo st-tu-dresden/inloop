ac007e061306b3f2ddf78723a1c0464858e31995
Martin Morgenstern <rm.morgenstern@gmail.com>
2015-06-04 18:37:32 +0200

    Reset migrations to completely remove dependency on tinymce

--- tasks/migrations/0001_initial.py
+++ tasks/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import tinymce.models
 from django.conf import settings
+import tasks.models
 
 
 class Migration(migrations.Migration):
@@ -16,17 +16,59 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Task',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(help_text=b'Task name', max_length=100)),
-                ('description', tinymce.models.HTMLField(help_text=b'Task description')),
-                ('publication_date', models.DateTimeField(help_text=b'When should the task be published?')),
-                ('deadline_date', models.DateTimeField(help_text=b'Date the task is due to')),
-                ('category', models.CharField(help_text=b'Category of task', max_length=1, choices=[(b'B', b'Basic Exercise'), (b'A', b'Advanced Exercise'), (b'L', b'Lesson Exercise'), (b'E', b'Exam Exercise')])),
-                ('slug', models.SlugField(help_text=b'URL name', unique=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('title', models.CharField(help_text='Task name', max_length=100)),
+                ('description', models.TextField(help_text='Task description')),
+                ('publication_date', models.DateTimeField(help_text='When should the task be published?')),
+                ('deadline_date', models.DateTimeField(help_text='Date the task is due to')),
+                ('slug', models.SlugField(help_text='URL name', unique=True)),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TaskCategory',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('short_id', models.CharField(help_text='Short ID for URLs', max_length=50, unique=True)),
+                ('name', models.CharField(help_text='Category Name', max_length=50, unique=True)),
+                ('image', models.ImageField(upload_to='images/category_thumbs/')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TaskSolution',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('submission_date', models.DateTimeField(help_text='When was the solution submitted?')),
+                ('is_correct', models.BooleanField(default=False, help_text='Did the checker accept the solution?')),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('task', models.ForeignKey(to='tasks.Task')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TaskSolutionFile',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('filename', models.CharField(max_length=50)),
+                ('file', models.FileField(upload_to=tasks.models.get_upload_path)),
+                ('solution', models.ForeignKey(to='tasks.TaskSolution')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='task',
+            name='category',
+            field=models.ForeignKey(to='tasks.TaskCategory'),
+            preserve_default=True,
         ),
     ]
