b2349157c6f5dba9e360ed1f2988e1b65b296afa
Martin Morgenstern <rm.morgenstern@gmail.com>
2015-06-04 19:20:14 +0200

    Use MEDIA_ROOT and make use of Python imports (fixes #60)

--- tasks/filesystem_utils.py
+++ tasks/filesystem_utils.py
@@ -1,66 +1,68 @@
-import shutil
-import glob
-import os
-from os import path, listdir
+from shutil import rmtree
+from glob import glob
+from os import listdir, makedirs, path, remove
+from os.path import basename, join
 
 from django.conf import settings
 
+EXERCISE_ROOT = join(settings.MEDIA_ROOT, 'exercises')
+
 
 def get_template_names(task_name):
-    p = path.join(settings.BASE_DIR, 'media', 'exercises', task_name)
+    p = join(EXERCISE_ROOT, task_name)
     # get all java template files for task, e.g. 'template.java'
-    return map(path.basename, glob.glob(p + path.sep + '*.java'))
+    return map(basename, glob(p + path.sep + '*.java'))
 
 
 def get_unittest_names(task_name):
-    p = path.join(settings.BASE_DIR, 'media', 'exercises', task_name, 'unittests')
+    p = join(EXERCISE_ROOT, task_name, 'unittests')
     # get all java template files for task, e.g. 'unittest.java'
-    return map(path.basename, glob.glob(p + path.sep + '*.java'))
+    return map(basename, glob(p + path.sep + '*.java'))
 
 
 def del_template(f_name, task_name):
-    p = path.join(settings.BASE_DIR, 'media', 'exercises', task_name)
-    os.remove(path.join(p, f_name))
+    p = join(EXERCISE_ROOT, task_name)
+    remove(join(p, f_name))
 
 
 def del_unittest(f_name, task_name):
-    p = path.join(settings.BASE_DIR, 'media', 'exercises', task_name, 'unittests')
-    os.remove(path.join(p, f_name))
+    p = join(EXERCISE_ROOT, task_name, 'unittests')
+    remove(path.join(p, f_name))
 
 
 def del_task_files(task_name):
-    p = path.join(settings.BASE_DIR, 'media', 'exercises', task_name)
-    shutil.rmtree(p)
+    p = join(EXERCISE_ROOT, task_name)
+    rmtree(p)
 
 
 def handle_uploaded_unittest(f, task_name):
-    p = path.join(settings.BASE_DIR, 'media', 'exercises', task_name, 'unittests')
+    p = join(EXERCISE_ROOT, task_name, 'unittests')
     if not path.exists(p):
-        os.makedirs(p)
+        makedirs(p)
 
-    with open(path.join(p, f.name), 'wb+') as destination:
+    with open(join(p, f.name), 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
 
 def handle_uploaded_exercise(f, task_name):
-    p = path.join(settings.BASE_DIR, 'media', 'exercises', task_name)
+    p = join(EXERCISE_ROOT, task_name)
     if not path.exists(p):
-        os.makedirs(p)
+        makedirs(p)
 
-    with open(path.join(p, f.name), 'wb+') as destination:
+    with open(join(p, f.name), 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
 
 def get_task_templates(task_name):
     overview = {}
-    p = path.join(settings.BASE_DIR, 'media', 'exercises', task_name)
+    p = join(EXERCISE_ROOT, task_name)
     # get all java template files for task
-    filenames = map(path.basename, glob.glob(p + path.sep + '*.java'))
+    filenames = map(basename, glob(p + path.sep + '*.java'))
     # build dict overview
     for fname in filenames:
-        with open(path.join(p, fname)) as f:
+        with open(join(p, fname)) as f:
             overview[fname] = f.read()
     return overview
 
@@ -74,26 +76,21 @@ def latest_solution_files(task, username):
     max_id_in_dir = lambda p: str(max([int(d[6:]) for d in listdir(p)]))
 
     overview = {}
-    p = path.join(
-        settings.BASE_DIR,
-        'media',
-        'solutions',
-        username,
-        task.title)
+    p = join(settings.MEDIA_ROOT, 'solutions', username, task.title)
 
     if path.exists(p):
         year = max_int_in_dir(p)
-        p = path.join(p, year)
+        p = join(p, year)
         month = leading_zero(max_int_in_dir(p))
-        p = path.join(p, month)
+        p = join(p, month)
         day = leading_zero(max_int_in_dir(p))
-        p = path.join(p, day)
+        p = join(p, day)
         time = filter(lambda x: str(x).endswith(max_id_in_dir(p)), listdir(p))
-        p = path.join(p, time[0])  # filter delivers one element list
+        p = join(p, time[0])  # filter delivers one element list
 
-        filenames = map(path.basename, glob.glob(p + path.sep + '*.java'))
+        filenames = map(basename, glob(p + path.sep + '*.java'))
         for fname in filenames:
-            with open(path.join(p, fname)) as f:
+            with open(join(p, fname)) as f:
                 overview[fname] = f.read()
     else:
         # display templates
