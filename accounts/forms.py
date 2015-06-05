87d3297ec98769f78bc88f03ad3959d978a40795
dmuhs <dominik.muhs@mailbox.tu-dresden.de>
2015-06-05 17:41:03 +0200

    enter password twice for registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,9 @@ from accounts.validators import validate_mat_num
 
 
 class UserForm(forms.ModelForm):
+    error_messages = {
+        'password_mismatch': 'The two password fields didn\'t match.',
+    }
     username = forms.CharField(
         widget=forms.TextInput(attrs={
             'class': 'form-control',
@@ -15,11 +18,17 @@ class UserForm(forms.ModelForm):
             'class': 'form-control',
             'autocomplete': 'off'
         }))
-    password = forms.CharField(
+    password1 = forms.CharField(
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
             'autocomplete': 'off'
         }))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'autocomplete': 'off'
+        })
+    )
     course = forms.ModelChoiceField(
         queryset=COS.objects.all())
     mat_num = forms.IntegerField(
@@ -31,9 +40,24 @@ class UserForm(forms.ModelForm):
         }),
         validators=[validate_mat_num])
 
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
     class Meta(object):
         model = UserProfile
-        fields = ('username', 'email', 'password', 'course', 'mat_num')
+        fields = ('username',
+                  'email',
+                  'password1',
+                  'password2',
+                  'course',
+                  'mat_num')
 
 
 class NewCourseForm(forms.ModelForm):
