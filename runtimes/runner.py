fe336e5761759e677af66b91af8f37f2f24661f7
Martin Morgenstern <mail@mmorgenstern.net>
2015-07-08 14:40:18 +0200

    Rewrite the runner implementation

--- runtimes/runner.py
+++ runtimes/runner.py
@@ -3,41 +3,28 @@ from subprocess import Popen
 from threading import Timer
 
 
-def killer(proc):
-    "Return a function that calls proc.kill()"
-    def _func():
-        proc.kill()
-    return _func
-
-
 @contextmanager
 def timelimit(timeout, function):
-    "Context manager which executes function after a timeout seconds"
+    """Context manager which executes function after timeout seconds."""
     t = Timer(timeout, function)
     t.start()
     yield
     t.cancel()
 
 
+class TimeoutExpired(Exception):
+    pass
+
+
 class BasicRunner:
-    """
-    Runs Popen objects with timeouts, in a sane environment and optionally a tmpdir.
-    """
-    def __init__(self, args, timeout=15, cwd=None):
-        self.args = args
+    """Simple runner that supports a timeout and uses a clean environment."""
+    def __init__(self, timeout=5, cwd=None):
         self.timeout = timeout
         self.cwd = cwd
 
-    def run(self):
-        proc = Popen(self.args, env={}, cwd=self.cwd)
-        with timelimit(self.timeout, killer(proc)):
-            # Note: in Python > 3.3, communicate() also supports a timeout parameter
-            stdout, stderr = proc.communicate()
-        return (stdout, stderr, proc.returncode)
-
-
-class DockerRunner(BasicRunner):
-    """
-    This will run commands inside a docker container ... some time in the future.
-    """
-    pass
+    def run(self, args):
+        proc = Popen(args, env={}, cwd=self.cwd)
+        with timelimit(self.timeout, lambda: proc.kill()):
+            outs, errs = proc.communicate()
+        # XXX: raise exception? (means we lose the output)
+        return (outs, errs, proc.returncode)
