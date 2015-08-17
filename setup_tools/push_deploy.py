7a6eca58b4b89c7e6bc22aad3cd5200851c92faf
Martin Morgenstern <mail@mmorgenstern.net>
2015-08-17 13:47:27 +0200

    Turn comments into prints and be quiet otherwise

--- setup_tools/push_deploy.py
+++ setup_tools/push_deploy.py
@@ -9,14 +9,16 @@ ARGS.add_argument("ssh_host", metavar="[user@]hostname", help="SSH host and opti
 ARGS.add_argument("remote_name", help="name that should be used for the new remote")
 args = ARGS.parse_args()
 
-# Initialize a bare repository to push to
-run(["ssh", args.ssh_host, "git init --bare ~/inloop.git"])
+print("Initializing a bare repository to push to.")
+run(["ssh", args.ssh_host, "git init -q --bare ~/inloop.git"])
 
-# Install the hook
-run(["scp", "support/git-hooks/post-receive", "%s:inloop.git/hooks" % args.ssh_host])
+print("Installing the hook.")
+run(["scp", "-q", "support/git-hooks/post-receive", "%s:inloop.git/hooks" % args.ssh_host])
 
-# Update the server's clone to pull from the local bare repository
+print("Updating the server's clone to pull from the bare repository.")
 run(["ssh", args.ssh_host, "cd ~/inloop && git remote set-url origin ~/inloop.git"])
 
-# Add the push-deploy remote
+print("Adding remote '%s' to your git repo." % args.remote_name)
 run(["git", "remote", "add", args.remote_name, "%s:inloop.git" % args.ssh_host])
+
+print("Done.")
