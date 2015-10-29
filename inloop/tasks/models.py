8a75610ee3cf38724ac0e3a8f339c54d584139f1
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2015-10-29 17:10:07 +0100

    Remove clutter

--- inloop/tasks/models.py
+++ inloop/tasks/models.py
@@ -158,7 +158,7 @@ class Checker:
 
     def start(self):
         # TODO: Give container unique name during execution
-        cmd = 'bash -c \"cd {loc}&&{test}'.format(loc=self.task_location, test=self.test_cmd)
+        cmd = 'bash -c \"cd {} && {}'.format(self.task_location, self.test_cmd)
         self._container_build('docker-test').decode()
         result = self._container_execute(cmd=shplit(cmd), ctr_tag='docker-test', ctr_name='test')
         result = result.decode()
