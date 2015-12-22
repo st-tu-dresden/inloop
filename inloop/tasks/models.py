2575744ba8bc1cf692bd7f8fd4185d6d1d5cf098
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2015-12-22 13:00:12 +0100

    Fix bug where re could not deal with byte strings

--- inloop/tasks/models.py
+++ inloop/tasks/models.py
@@ -283,8 +283,8 @@ class Checker:
         if not result:
             logging.debug("_parse_result got an empty result")
         else:
-            time = float(re.findall('Total time:\s(\d+\.\d+)\s\w+', result)[0])
-            passed = False if re.findall('BUILD (\w+)', result)[0] == 'FAILED' else True
+            time = float(re.findall('Total time:\s(\d+\.\d+)\s\w+', result.decode())[0])
+            passed = False if re.findall('BUILD (\w+)', result.decode())[0] == 'FAILED' else True
             cr = CheckerResult(
                 solution=self.solution,
                 result=ContentFile(result),
