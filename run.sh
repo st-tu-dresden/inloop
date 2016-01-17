f7cbcc9c6080fa2caeed4e8f7f2ddff0e910cc27
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2016-01-17 15:29:14 +0100

    Add example and TODO to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
+# e.g. run.sh basic-library
+# TODO: Add solutionPath property to build.gradle
 cd "$1"
-../gradlew -PsolutionPath="$2" -q test >/dev/null 2>&1
+../gradlew -PsolutionPath=/home/gradle/solution/ -q test >/dev/null 2>&1
 rc=$?
 cat build/test-results/*.xml
 exit $rc
