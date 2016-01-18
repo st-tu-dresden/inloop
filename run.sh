4ca08756e5eaa7344f47d346be338010e7d23ba3
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2016-01-18 08:27:42 +0100

    Gradle works inside containers - don't forget to use slugified task names in media/

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 # e.g. run.sh basic-library
 # TODO: Add solutionPath property to build.gradle
-cd "$1"
+echo $(pwd)
+echo "$1"
+cd "exercises/$1"
 ../gradlew -PsolutionPath=/home/gradle/solution/ -q test >/dev/null 2>&1
 rc=$?
-cat build/test-results/*.xml
+cat /home/gradle/solution/build/test-results/*.xml
 exit $rc
