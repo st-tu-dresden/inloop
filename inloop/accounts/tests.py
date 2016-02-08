31eed1ac8cb0aea8ee9c7e9213061571e11c5268
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2016-02-08 01:58:25 +0100

    Refactored test user data to setUp method

--- inloop/accounts/tests.py
+++ inloop/accounts/tests.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AnonymousUser
 
 from inloop.accounts.models import UserProfile, CourseOfStudy
 from inloop.accounts.forms import UserForm
-from inloop.tasks.views import index
+import inloop.accounts.views as account_views
+import inloop.tasks.views as task_views
 
 
 class LoginSystemTests(TestCase):
@@ -11,6 +12,16 @@ class LoginSystemTests(TestCase):
         self.factory = RequestFactory()
         self.password = '123456'
         self.course = CourseOfStudy.objects.create(name='test_course')
+        self.data = {
+            'username': 'john',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'john@example.com',
+            'password': 'abc123456',
+            'password_repeat': 'abc123456',
+            'course': self.course.id,
+            'mat_num': '1234567'
+        }
         self.user = UserProfile.objects.create_user(
             username='test_user',
             first_name='first_name',
@@ -21,17 +32,7 @@ class LoginSystemTests(TestCase):
         )
 
     def test_registration_password_consistency(self):
-        data = {
-            'username': 'john',
-            'first_name': 'John',
-            'last_name': 'Doe',
-            'email': 'john@example.com',
-            'password': 'abc123456',
-            'password_repeat': 'abc123456',
-            'course': self.course.id,
-            'mat_num': '1234567'
-        }
-        uf = UserForm(data)
+        uf = UserForm(self.data)
         self.assertTrue(uf.is_valid())
         user = uf.save(commit=False)
         user.set_password(uf.cleaned_data['password'])
@@ -39,16 +40,16 @@ class LoginSystemTests(TestCase):
         user.save()
 
         # Test login with fresh user
-        resp = self.client.post('/accounts/login/', data=data, follow=True)
+        resp = self.client.post('/accounts/login/', data=self.data, follow=True)
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(resp.context['user'].is_authenticated())
-        self.assertEqual(resp.context['user'].get_username(), data['username'])
+        self.assertEqual(resp.context['user'].get_username(), self.data['username'])
         self.assertContains(resp, '>john<')
 
     def test_anonymous_login_form(self):
         request = self.factory.get('/')
         request.user = AnonymousUser()
-        response = index(request)
+        response = task_views.index(request)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '>Login<')
 
