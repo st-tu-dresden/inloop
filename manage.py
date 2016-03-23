b4f883bb083cbcee42ee0cc0435a91bab9314444
Martin Morgenstern <mail@mmorgenstern.net>
2016-03-23 12:29:26 +0100

    Revert "Silence the 'imp' deprecation warning"
    Doesn't apply anymore to Django 1.8.x.
    
    This reverts commit f84ad08fea186dc46b191529761ff7876c0bb4a3.

--- manage.py
+++ manage.py
@@ -6,8 +6,6 @@ import warnings
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inloop.settings.development")
     warnings.simplefilter("default")
-    warnings.filterwarnings("ignore", "the imp module is deprecated",
-                            PendingDeprecationWarning, "imp")
 
     from django.core.management import execute_from_command_line
 
