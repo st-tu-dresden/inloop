d15db51c4c83654ffbc660184d46ee8e3cd10f19
Martin Morgenstern <mail@mmorgenstern.net>
2016-03-23 16:53:00 +0100

    Clean up whitespace and unnecessary comments

--- inloop/urls.py
+++ inloop/urls.py
@@ -1,4 +1,3 @@
-# To serve static files during development
 from django.conf import settings
 from django.conf.urls import include, patterns, url
 from django.conf.urls.static import static
@@ -16,6 +15,7 @@ urlpatterns = patterns(
     url(r'^github/', include('inloop.gh_import.urls', namespace='github')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
 # serve static content
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
