bc843f623c6c3f48adbeafdafaf09a10677cc4d1
Martin Morgenstern <mail@mmorgenstern.net>
2016-04-04 08:42:55 +0200

    Adjust admin site listing for Tasks
    Since we import using the 'system' user, displaying the author makes no
    sense. Instead we display and allow filtering using task categories.

--- inloop/tasks/admin.py
+++ inloop/tasks/admin.py
@@ -11,8 +11,8 @@ class TaskAdmin(admin.ModelAdmin):
                                          'deadline_date']}),
         ('Content', {'fields': ['description', 'slug']})
     ]
-    list_display = ('title', 'publication_date', 'deadline_date', 'author')
-    list_filter = ['publication_date', 'deadline_date']
+    list_display = ('title', 'category', 'publication_date', 'deadline_date')
+    list_filter = ['publication_date', 'deadline_date', 'category']
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
 
