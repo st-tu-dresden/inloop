8eee8d6258a29f86d2654a4e37502a411ec3d577
Martin Morgenstern <mail@mmorgenstern.net>
2016-04-04 08:35:46 +0200

    Improve admin site listing of UserProfiles

--- inloop/accounts/admin.py
+++ inloop/accounts/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from inloop.accounts.models import CourseOfStudy, UserProfile
 
-# Register your models here.
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name',
+                    'mat_num', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ['is_active', 'is_staff', 'is_superuser']
+    search_fields = ['last_name', 'email', 'mat_num']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(CourseOfStudy)
