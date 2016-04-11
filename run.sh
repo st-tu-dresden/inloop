e4d003cfbedfac699eb7dfa6e2f644f25b3f1a81
Martin Morgenstern <mail@mmorgenstern.net>
2016-04-11 13:55:57 +0200

    Fix major bugs in the deprecated run.sh script

--- run.sh
+++ run.sh
@@ -1,14 +1,43 @@
-# e.g. run.sh basic-library
-cd "exercises/$1"
-../gradlew -PsolutionPath=/home/gradle/solution/ -q test >/dev/null 2>&1
-rc=$?
-cat build/test-results/*.xml 2>/dev/null
-if [ "$?" -ne 0 ]
-  then
-    # Test reports don't exist. Assume a compiler error
-    # happened and call javac manually for error output
-    # cd into directory to hide path in stdout error report
-    cd /home/gradle/solution/ && javac *
-    rc=42
+# e.g. run.sh BasicLibrary
+
+# NOTE
+# this stuff and the ol' checker will be removed soon, but until the new
+# one gets deployed we have to fix the most annoying bugs here.
+
+
+# yes, the only return code that will mark a solution as not passed is 42
+cd "exercises/$1" || exit 42
+
+solution_path=/home/gradle/solution
+
+# try to show a friendly message if students use the package statement
+if grep -q '^package ' $solution_path/*.java; then
+    echo 'You are using the `package` statement, but our checker expects'
+    echo 'all classes to be in the default package. In other words, please'
+    echo 'remove the `package` statement and upload your solution again.'
+    exit 42
 fi
-exit $rc
+
+# compile separately while redirecting compiler errors to stdout, disguise internal path in output
+../gradlew -q --console=plain -PsolutionPath=$solution_path compileTestJava 2>&1 \
+    | sed "s|$solution_path|/sandbox|g"
+
+# compiler error -> return 42
+if [ $? -ne 0 ]; then
+    echo "NOT PASSED - COMPILATION FAILED."
+    exit 42
+fi
+
+# execute the tests & ignore output ...
+../gradlew -q -PsolutionPath=$solution_path test >/dev/null 2>&1
+rc=$?
+
+# ... because we use the XML results
+cat build/test-results/*.xml 2>/dev/null
+
+if [ $? -ne 0 ] && [ $rc -ne 0 ]; then
+    echo "Your solution failed the test, but I can't find any test reports "
+    echo "that would assist you while debugging the problem :-("
+    # ensure passed will be set to False:
+    exit 42
+fi
