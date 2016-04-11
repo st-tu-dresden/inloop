eb828e2bea801fec554a3bd7f13ea81fe5700a7c
Martin Morgenstern <mail@mmorgenstern.net>
2016-04-11 22:26:48 +0200

    Refactoring

--- inloop/tasks/test_docker_subproc.py
+++ inloop/tasks/test_docker_subproc.py
@@ -47,6 +47,8 @@ class DockerSubProcessCheckerTests(TestCase):
     # name of the Docker image we use for this test
     image_name = "inloop-integration-test"
 
+    input_path = TEST_DIR
+
     # docker-machine on OS X needs a mount point below $HOME
     options = {
         "timeout": 1.5,
@@ -73,7 +75,10 @@ class DockerSubProcessCheckerTests(TestCase):
 
     def test_outputs(self):
         """Test if we receive stdout, stderr and exit code."""
-        result = self.checker.check_task("echo -n OUT; echo -n ERR >&2; exit 42", TEST_DIR)
+        result = self.checker.check_task(
+            "echo -n OUT; echo -n ERR >&2; exit 42",
+            self.input_path
+        )
         self.assertEqual(result.rc, 42)
         self.assertEqual(result.stdout, "OUT")
         self.assertEqual(result.stderr, "ERR")
@@ -81,48 +86,66 @@ class DockerSubProcessCheckerTests(TestCase):
 
     def test_kill_on_timeout(self):
         """Test if the container gets killed after the timeout."""
-        result = self.checker.check_task("sleep 10", TEST_DIR)
+        result = self.checker.check_task("sleep 10", self.input_path)
         self.assertEqual(result.rc, signal.SIGKILL)
         self.assertGreaterEqual(result.duration, 0.0)
         self.assertLess(result.duration, 10.0)
 
     def test_output_on_timeout(self):
         """Test if we receive output even if a timeout happens."""
-        result = self.checker.check_task("echo -n OUT; echo -n ERR >&2; sleep 10", TEST_DIR)
+        result = self.checker.check_task(
+            "echo -n OUT; echo -n ERR >&2; sleep 10",
+            self.input_path
+        )
         self.assertEqual(result.rc, signal.SIGKILL)
         self.assertEqual(result.stdout, "OUT")
         self.assertEqual(result.stderr, "ERR")
 
     def test_inbound_mountpoint(self):
         """Test if the input mount point works correctly."""
-        result = self.checker.check_task("cat /checker/input/docker/Dockerfile", TEST_DIR)
+        result = self.checker.check_task(
+            "cat /checker/input/docker/Dockerfile",
+            self.input_path
+        )
         self.assertIn("FROM alpine", result.stdout)
         self.assertEqual(result.rc, 0)
 
     def test_scratch_area(self):
         """Test that we can write the scratch area."""
-        result = self.checker.check_task("touch /checker/scratch/test_file", TEST_DIR)
+        result = self.checker.check_task(
+            "touch /checker/scratch/test_file",
+            self.input_path
+        )
         self.assertEqual(result.rc, 0)
 
     def test_inbound_mountpoint_ro(self):
         """Test if the input is mounted read-only."""
-        result = self.checker.check_task("touch /checker/input/test_file", TEST_DIR)
+        result = self.checker.check_task(
+            "touch /checker/input/test_file",
+            self.input_path
+        )
         self.assertNotEqual(result.rc, 0)
 
     def test_storage_exists(self):
         """Test if the storage directory exists."""
-        result = self.checker.check_task("test -d /checker/output/storage", TEST_DIR)
+        result = self.checker.check_task(
+            "test -d /checker/output/storage",
+            self.input_path
+        )
         self.assertEqual(result.rc, 0)
 
     def test_output_filedict(self):
         """Test if we can create a file which appears in the file_dict."""
-        result = self.checker.check_task("echo -n FOO >/checker/output/storage/bar", TEST_DIR)
+        result = self.checker.check_task(
+            "echo -n FOO >/checker/output/storage/bar",
+            self.input_path
+        )
         self.assertEqual(result.rc, 0)
         self.assertEqual("FOO", result.file_dict["bar"])
 
     def test_container_unprivileged(self):
         """Test if we execute commands as unprivileged user."""
-        result = self.checker.check_task("id -un", TEST_DIR)
+        result = self.checker.check_task("id -un", self.input_path)
         self.assertEqual(result.rc, 0)
         self.assertEqual(result.stdout.strip(), "nobody")
 
@@ -130,6 +153,6 @@ class DockerSubProcessCheckerTests(TestCase):
         """Test file size limits."""
         result = self.checker.check_task(
             "dd if=/dev/zero of=/checker/scratch/largefile bs=1M count=100",
-            TEST_DIR
+            self.input_path
         )
         self.assertNotEqual(result.rc, 0)
