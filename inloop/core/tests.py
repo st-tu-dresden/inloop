9a3e84bcabe0cb16933c9522887cc3cfa35ed847
Martin Morgenstern <mail@mmorgenstern.net>
2016-04-12 11:31:00 +0200

    Remove context managers never used anywhere

--- inloop/core/tests.py
+++ inloop/core/tests.py
@@ -1,34 +1,9 @@
-import os
-import subprocess
-import sys
-from unittest import TestCase, mock
+from unittest import TestCase
 
-from inloop.core.utils import changedir, filter_uppercase_keys, timelimit
+from inloop.core.utils import filter_uppercase_keys
 
 
 class UtilsTest(TestCase):
     def test_filter_vars(self):
         d = {'foo': 'bar', 'FOO': 'bar', 'KEY': 'value'}
         self.assertEqual(filter_uppercase_keys(d), {'FOO': 'bar', 'KEY': 'value'})
-
-
-class ContextManagerTests(TestCase):
-    class TestException(Exception):
-        """Exception used in some chdir tests."""
-        pass
-
-    def test_chdir_exception(self):
-        """Test if cwd gets restored even if an exception occurs in the with block."""
-        with mock.patch("inloop.core.utils.os.chdir") as patched_chdir:
-            with self.assertRaises(self.TestException):
-                current_dir = os.getcwd()
-                with changedir("/some/path"):
-                    patched_chdir.assert_called_with("/some/path")
-                    raise self.TestException
-            patched_chdir.assert_called_with(current_dir)
-
-    def test_timelimit(self):
-        proc = subprocess.Popen([sys.executable, '-c', "while True: pass"])
-        with timelimit(0.01, lambda: proc.kill()):
-            proc.communicate()
-        self.assertEqual(proc.returncode, -9)
