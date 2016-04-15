e503fc357e9422842f0c05d521a0d8c20332385a
Martin Morgenstern <mail@mmorgenstern.net>
2016-04-15 11:24:38 +0200

    Add new classbased TaskDetailView with new HTML
    The task description will be displayed on its own page, while upload and
    solution overview will be handled in other views.
    
    Breadcrumb navigation and the tab bar can be reused through template
    inheritance.

--- inloop/tasks/urls.py
+++ inloop/tasks/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 
 from inloop.tasks import views
-from inloop.tasks.views_new import SolutionStatusView
+from inloop.tasks.views_new import SolutionStatusView, TaskDetailView
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^category/(?P<slug>[-\w]+)/$', views.category, name='category'),
     url(r'^solution/(?P<solution_id>[\d]+)/status$',
         SolutionStatusView.as_view(), name='solutionstatus'),
-    url(r'^(?P<slug>[-\w]+)/$', views.detail, name='detail'),
+    url(r'^(?P<slug>[-\w]+)/$', TaskDetailView.as_view(), name='detail'),
     url(r'^(?P<slug>[-\w]+)/results/(?P<solution_id>[\d]+)$', views.results, name='results'),
     url(r'^(?P<slug>[-\w]+)/download_solution/(?P<solution_id>[\d]+)/$',
         views.get_solution_as_zip, name='solution_as_zip'),
--- inloop/tasks/views_new.py
+++ inloop/tasks/views_new.py
@@ -4,15 +4,22 @@ Class based views to manage tasks, task categories and submitted solutions.
 from django import VERSION as DJANGO_VERSION
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import Http404, get_object_or_404, render
 from django.views.generic import View
 
-from inloop.tasks.models import TaskSolution
+from inloop.tasks.models import Task, TaskSolution
 
 
 # Once we have upgraded to Django 1.9, we can use the shipped contrib.auth Mixins.
 if DJANGO_VERSION >= (1, 9, 0):
     raise DeprecationWarning("LoginRequiredMixin is provided by Django")
+
+
+def get_active_task_or_404(slug):
+    task = get_object_or_404(Task, slug=slug)
+    if not task.is_active():
+        raise Http404
+    return task
 
 
 class LoginRequiredMixin:
@@ -27,3 +34,10 @@ class SolutionStatusView(LoginRequiredMixin, View):
     def get(self, request, solution_id):
         solution = get_object_or_404(TaskSolution, pk=solution_id, author=request.user)
         return JsonResponse({'solution_id': solution.id, 'status': solution.status()})
+
+
+class TaskDetailView(LoginRequiredMixin, View):
+    def get(self, request, slug):
+        return render(request, "tasks/detail.html", {
+            'task': get_active_task_or_404(slug=slug)
+        })
