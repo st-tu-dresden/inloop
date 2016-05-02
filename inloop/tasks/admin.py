6655197cf8857d9783bc1e5826d39b4f79f64add
Martin Morgenstern <mail@mmorgenstern.net>
2016-05-02 19:48:28 +0200

    Add links to on-site detail view of solutions

--- inloop/tasks/admin.py
+++ inloop/tasks/admin.py
@@ -42,7 +42,7 @@ class TaskSolutionAdmin(admin.ModelAdmin):
         TaskSolutionFileInline,
         CheckerResultInline
     ]
-    list_display = ('id', 'author', 'task', 'submission_date', 'passed', 'view_on_site')
+    list_display = ('id', 'author', 'task', 'submission_date', 'passed', 'site_link')
     list_filter = ['passed', 'task']
     search_fields = [
         'author__username',
@@ -51,6 +51,12 @@ class TaskSolutionAdmin(admin.ModelAdmin):
         'author__last_name'
     ]
     readonly_fields = ('task', 'submission_date', 'task', 'author', 'passed')
+
+    def site_link(self, obj):
+        return '<a href="%s">%s details</a>' % (obj.get_absolute_url(), obj)
+
+    site_link.allow_tags = True
+    site_link.short_description = "View on site"
 
 
 class CheckerOutputInline(admin.TabularInline):
