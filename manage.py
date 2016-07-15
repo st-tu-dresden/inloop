c816acb5b7b2fa348ccdeb1319e21d6d85e4d81b
Martin Morgenstern <mail@mmorgenstern.net>
2016-07-15 14:54:41 +0200

    Restore Python's default warning behavior

--- manage.py
+++ manage.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-import warnings
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inloop.settings.development")
-    warnings.simplefilter("default")
-    warnings.filterwarnings("ignore", "the imp module is deprecated",
-                            PendingDeprecationWarning, "imp")
 
     from django.core.management import execute_from_command_line
 
