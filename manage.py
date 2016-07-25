15437ff87de0d0fd31cf4a60038640ec79242020
Martin Morgenstern <mail@mmorgenstern.net>
2016-07-25 21:08:49 +0200

    Add tests.settings module with overrides for unit tests

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import sys
 from environ import Env
 
@@ -7,6 +8,8 @@ if __name__ == "__main__":
 
     if sys.argv[1] == "shell":
         sys.argv[1] = "shell_plus"
+    elif "test" in sys.argv:
+        os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
 
     from django.core.management import execute_from_command_line
 
---
+++ tests/settings.py
@@ -0,0 +1,13 @@
+import warnings
+warnings.simplefilter("always")
+
+from inloop.settings import *   # noqa
+
+HUEY = {
+    "always_eager": True,
+}
+
+PASSWORD_HASHERS = [
+    # speed up tests involving user authentication
+    "django.contrib.auth.hashers.SHA1PasswordHasher",
+]
