670daa1f4b66a68afe8e703acb22c9db23e7f50f
Martin Morgenstern <mail@mmorgenstern.net>
2016-11-30 13:07:40 +0100

    Polishing

--- inloop/core/templatetags/markdown.py
+++ inloop/core/templatetags/markdown.py
@@ -1,23 +1,20 @@
-from django import template
+from django.template import Library
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 
 from markdown import Markdown
 from markdown.extensions.codehilite import CodeHiliteExtension
 
-register = template.Library()
-_markdown = Markdown(
-    output_format='html5',
-    extensions=[
-        "markdown.extensions.toc",
-        "markdown.extensions.smarty",
-        "markdown.extensions.fenced_code",
-        CodeHiliteExtension(use_pygments=False)
-    ]
-)
+register = Library()
+convert = Markdown(output_format="html5", extensions=[
+    "markdown.extensions.toc",
+    "markdown.extensions.smarty",
+    "markdown.extensions.fenced_code",
+    CodeHiliteExtension(use_pygments=False)
+]).convert
 
 
 @register.filter(is_safe=True)
 @stringfilter
 def markdown(value):
-    return mark_safe(_markdown.convert(value))
+    return mark_safe(convert(value))
