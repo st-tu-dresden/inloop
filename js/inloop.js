709023450accfa8f5b357872bc7760c97ad4ac75
Martin Morgenstern <mail@mmorgenstern.net>
2016-11-30 20:13:05 +0100

    Update solution status url used in AJAX requests

--- js/inloop.js
+++ js/inloop.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   anchors.add(".autolink h2, .autolink h3");
 
   $(".solution-pending").refreshJSON("activate", {
-    url: "/tasks/solution/:id:/status",
+    url: "/solutions/:id:/status",
     interval: 5000,
     success: function(data) {
       $(this).attr("class", "glyphicon solution-" + data.status);
