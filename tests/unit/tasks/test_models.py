f15e8cdd16d1503bc57489b893e341b5bcfaa401
Martin Morgenstern <mail@mmorgenstern.net>
2016-12-01 18:14:48 +0100

    Finish the task model tests

--- tests/unit/tasks/test_models.py
+++ tests/unit/tasks/test_models.py
@@ -3,9 +3,11 @@ from doctest import DocTestSuite
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
+from inloop.solutions.models import Solution
 from inloop.tasks import models
 from inloop.tasks.models import Category, Task
 
+from tests.unit.accounts.mixins import SimpleAccountsData
 from tests.unit.tasks.mixins import TaskData
 
 
@@ -28,55 +30,55 @@ class TaskTests(TaskData, TestCase):
             Task.objects.create(deadline="abc")
 
 
-class TaskCategoryTests(TestCase):
+class TaskCategoryTests(SimpleAccountsData, TaskData, TestCase):
     def test_slugify_on_save(self):
-        cat_name = "Whitespace here and 123 some! TABS \t - \"abc\" (things)\n"
-        slug = "whitespace-here-and-123-some-tabs-abc-things"
-        category = self.create_category(name=cat_name)
-        self.assertEqual(category.slug, slug)
+        category = Category.objects.create(name="Test category")
+        self.assertEqual(category.slug, "test-category")
 
     def test_completed_tasks_empty_category(self):
-        empty_cat = self.create_category(name="empty")
-        self.assertFalse(empty_cat.completed_tasks_for_user(self.user).exists())
+        empty_category = Category.objects.create(name="Empty category")
+        for user in [self.bob, self.alice]:
+            self.assertFalse(empty_category.completed_tasks_for_user(user).exists())
 
     def test_tasks_uncompleted(self):
-        solution = self.create_solution()
-        self.assertFalse(solution.passed)
-        self.assertFalse(self.cat.completed_tasks_for_user(self.user).exists())
+        Solution.objects.create(author=self.bob, task=self.published_task1)
+        self.assertFalse(self.category1.completed_tasks_for_user(self.bob).exists())
 
-    def test_tasks_completed_multiple_solutions(self):
-        solution1 = self.create_solution(passed=True)
-        solution2 = self.create_solution(passed=True)
-        self.assertTrue(solution1.passed)
-        self.assertTrue(solution2.passed)
-        self.assertEqual(self.cat.completed_tasks_for_user(self.user).count(), 1)
-        self.assertEqual(self.cat.completed_tasks_for_user(self.user)[0], self.task)
+    def test_tasks_completed(self):
+        Solution.objects.create(author=self.bob, task=self.published_task1, passed=True)
+        self.assertTrue(self.category1.completed_tasks_for_user(self.bob).exists())
 
-        task2 = self.create_task(title="Another task")
-        solution3 = self.create_solution(task=task2)
-        self.assertFalse(solution3.passed)
-        self.assertEqual(self.cat.completed_tasks_for_user(self.user).count(), 1)
+    def test_completed_same_task(self):
+        for i in range(2):
+            Solution.objects.create(author=self.bob, task=self.published_task1, passed=True)
+        completed = self.category1.completed_tasks_for_user(self.bob)
+        self.assertSequenceEqual(completed, [self.published_task1])
 
-        solution4 = self.create_solution(task=task2, passed=True)
-        self.assertTrue(solution4.passed)
-        self.assertEqual(self.cat.completed_tasks_for_user(self.user).count(), 2)
+    def test_completed_different_tasks(self):
+        for i in range(2):
+            Solution.objects.create(author=self.bob, task=self.published_task1, passed=True)
+            Solution.objects.create(author=self.bob, task=self.published_task2, passed=True)
+        completed = self.category1.completed_tasks_for_user(self.bob)
+        self.assertSequenceEqual(completed, [self.published_task1, self.published_task2])
 
 
-class TaskCategoryManagerTests(TestCase):
+class GetOrCreateTests(TestCase):
     def setUp(self):
         Category.objects.create(name="Test category")
 
     def test_returns_existing_category(self):
-        self.assertEqual(Category.objects.count(), 1)
+        before = Category.objects.count()
         category = Category.objects.get_or_create("Test category")
+        after = Category.objects.count()
         self.assertEqual(category.name, "Test category")
-        self.assertEqual(Category.objects.count(), 1)
+        self.assertEqual(before, after)
 
     def test_returns_new_category(self):
-        self.assertEqual(Category.objects.count(), 1)
+        before = Category.objects.count()
         category = Category.objects.get_or_create("Another category")
+        after = Category.objects.count()
         self.assertEqual(category.name, "Another category")
-        self.assertEqual(Category.objects.count(), 2)
+        self.assertEqual(before, after - 1)
 
 
 class TaskManagerTests(TestCase):
@@ -101,7 +103,7 @@ class TaskManagerTests(TestCase):
         self.assertEqual(task.slug, "test-title")
         self.assertEqual(task.category.name, "Lesson")
 
-        pubdate = task.publication_date.strftime("%Y-%m-%d %H:%M:%S")
+        pubdate = task.pubdate.strftime("%Y-%m-%d %H:%M:%S")
         self.assertEqual(pubdate, self.valid_json["pubdate"])
 
     def test_save_task_with_valid_json(self):
