ccaa5deaea3bd5d2b82d17856a520c900d75bf3d
Martin Morgenstern <mail@mmorgenstern.net>
2016-12-01 14:11:24 +0100

    Add new-style account models using OneToOne relations

--- inloop/accounts/models.py
+++ inloop/accounts/models.py
@@ -1,76 +1,30 @@
 from django.conf import settings
-from django.contrib.auth import models as auth_models
-from django.contrib.sites.shortcuts import get_current_site
 from django.db import models
-from django.template.loader import render_to_string
-from django.utils import timezone
-from django.utils.crypto import get_random_string
-
-from inloop.accounts.validators import validate_mat_num
 
 
-def next_week():
-    """To be used as the default activation key expiration time."""
-    return timezone.now() + timezone.timedelta(weeks=1)
+class Course(models.Model):
+    """Course of study."""
 
-
-class CourseOfStudy(models.Model):
-    """Represents the course of study of the respective user."""
-
-    class Meta:
-        verbose_name_plural = "Courses of study"
-
-    name = models.CharField(max_length=50, help_text="The course name", unique=True)
+    name = models.CharField(max_length=64, unique=True)
 
     def __str__(self):
         return self.name
 
 
-class UserProfile(auth_models.AbstractUser):
-    """Extended user model with support for course of studies."""
+def default_course():
+    course, _ = Course.objects.get_or_create(name="Other")
+    return course.id
 
-    # Length of the random key
-    KEY_LENGTH = 40
 
-    # FIXME: use activation key which encodes expiration
-    activation_key = models.CharField(
-        max_length=KEY_LENGTH,
-        help_text="Random key used to verify the user's email",
-        blank=True
-    )
-    key_expires = models.DateTimeField(
-        default=next_week,
-        blank=True,
-        help_text="The key's deprecation time"
-    )
-    mat_num = models.IntegerField(
-        help_text="Matriculation number",
-        null=True,
-        validators=[validate_mat_num]
-    )
-    course = models.ForeignKey(CourseOfStudy, null=True, on_delete=models.CASCADE)
+class StudentDetails(models.Model):
+    """Associate additional data to the user account."""
 
-    def generate_activation_key(self):
-        self.activation_key = get_random_string(length=self.KEY_LENGTH)
+    class Meta:
+        verbose_name_plural = "Student details"
 
-    def activate(self):
-        success = False
-        if self.key_expires > timezone.now():
-            self.is_active = True
-            success = True
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    matnum = models.CharField(blank=True, max_length=20)
+    course = models.ForeignKey(Course, default=default_course, on_delete=models.PROTECT)
 
-        self.activation_key = ''
-        self.save()
-        return success
-
-    def send_activation_mail(self, request):
-        context = {
-            "request": request,
-            "site": get_current_site(request),
-            "user": self
-        }
-        subject = render_to_string("registration/activation_email_subject.txt", context=context)
-        # force the subject to a single line
-        subject = "".join(subject.splitlines())
-        message = render_to_string("registration/activation_email.txt", context=context)
-        self.email_user(subject, message, settings.DEFAULT_FROM_EMAIL)
+    def __str__(self):
+        return self.user.username
