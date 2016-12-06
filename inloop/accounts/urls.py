595c090ad870a691084aa4e74c7e61f00aaec0e1
Martin Morgenstern <mail@mmorgenstern.net>
2016-12-06 13:34:50 +0100

    Add templates for django-registration based views

--- inloop/accounts/urls.py
+++ inloop/accounts/urls.py
@@ -6,7 +6,7 @@ app_name = "accounts"
 
 urlpatterns = [
     url(r'^activate/$', views.activate, name="activate"),
-    url(r'^create/$', views.register, name="register"),
+    url(r'^create/$', views.register, name="signup"),
     url(r'^profile/$', views.profile, name="profile"),
     url(r'^password/$', views.password_change, name="password_change"),
 ]
