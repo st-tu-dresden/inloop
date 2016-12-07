6d191ca2d13e1ac8e5cdbc4c7c0806a9b733dcf5
Martin Morgenstern <mail@mmorgenstern.net>
2016-12-07 18:53:44 +0100

    Show form help texts

--- inloop/accounts/forms.py
+++ inloop/accounts/forms.py
@@ -1,7 +1,21 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, password_validation
+
+from registration.forms import RegistrationFormUniqueEmail
 
 from inloop.accounts.models import StudentDetails
+
+
+class SignupForm(RegistrationFormUniqueEmail):
+    # Workaround for Django ticket #26097 until 1.11 is released:
+    # We need to override this field to include the help text of the
+    # configured AUTH_PASSWORD_VALIDATORS.
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html()
+    )
 
 
 class StudentDetailsForm(forms.ModelForm):
