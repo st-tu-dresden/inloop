a2e377afe4187692a0a9c3fcf6b90add45188e9f
Martin Morgenstern <mail@mmorgenstern.net>
2016-12-08 14:07:18 +0100

    Don't automatically append slashes to URLs

--- inloop/settings.py
+++ inloop/settings.py
@@ -64,6 +64,7 @@ LOGIN_REDIRECT_URL = reverse_lazy("home")
 LOGIN_URL = reverse_lazy("home")
 
 ROOT_URLCONF = "inloop.urls"
+APPEND_SLASH = False
 WSGI_APPLICATION = "inloop.wsgi.application"
 
 if DEBUG and env.bool("DJDT", default=False):
