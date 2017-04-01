af918dcebc2454692a74b83a83e9973ef406a365
Martin Morgenstern <mail@mmorgenstern.net>
2017-04-01 18:06:22 +0200

    Re-enable AnchorJS for the task detail view

--- assets/js/inloop.js
+++ assets/js/inloop.js
@@ -1,16 +1,3 @@
 $(document).ready(function() {
   anchors.add(".autolink h2, .autolink h3");
-
-  $(".solution-pending").refreshJSON("activate", {
-    url: "/solutions/:id:/status",
-    interval: 5000,
-    success: function(data) {
-      $(this).attr("class", "glyphicon solution-" + data.status);
-      if (data.status != "pending") {
-        $(this).refreshJSON("deactivate");
-      }
-    }
-  });
-
-  $(".timeago").timeago();
 });
