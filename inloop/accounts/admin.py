ba04e88ff6b73b8571f9cd79d321f4e7f38d31e2
Martin Morgenstern <mail@mmorgenstern.net>
2017-04-04 15:40:54 +0200

    Enhance the change view for StudentDetails

--- inloop/accounts/admin.py
+++ inloop/accounts/admin.py
@@ -8,6 +8,8 @@ class StudentDetailsAdmin(admin.ModelAdmin):
     list_display = ["user", "email", "first_name", "last_name", "matnum", "course"]
     list_filter = ["course"]
     search_fields = ["user__first_name", "user__last_name", "user__email", "matnum"]
+    fields = ["user", "email", "first_name", "last_name", "matnum", "course"]
+    readonly_fields = ["user", "email", "first_name", "last_name"]
 
     def first_name(self, obj):
         return obj.user.first_name
