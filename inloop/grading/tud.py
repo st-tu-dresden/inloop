90c0c1f9f838c95b82672c4ed0668158a86d3050
Martin Morgenstern <mail@mmorgenstern.net>
2017-07-21 10:59:41 +0200

    Add first code to support calculating grades

---
+++ inloop/grading/tud.py
@@ -0,0 +1,71 @@
+"""
+Bonus point grading specific to TU Dresden.
+"""
+
+import re
+
+from django.db.models import ObjectDoesNotExist
+from django.utils.timezone import make_aware
+
+from inloop.tasks.models import Category
+
+
+def get_user_data(user):
+    """
+    Return student data as a tuple (empty fields are guessed if possible).
+    """
+    try:
+        matnum = user.studentdetails.matnum
+        course = user.studentdetails.course
+    except ObjectDoesNotExist:
+        matnum = course = ""
+    if not user.last_name:
+        first_name, last_name = guess_name_from_email(user.email)
+    else:
+        first_name, last_name = user.first_name, user.last_name
+    return (last_name, first_name, user.email, matnum, course)
+
+
+def guess_name_from_email(email):
+    """
+    Try to guess first and last name from email and return it as a tuple.
+    """
+    name, domain = email.split("@", 1)
+    if domain != "mailbox.tu-dresden.de":
+        return "", ""
+    first_name, last_name = name.split(".", 1)
+    first_name = first_name.replace("_", " ")
+    last_name = last_name.replace("_", " ")
+    last_name = re.sub(r"[1-9]$", "", last_name)
+    return first_name.capitalize(), last_name.capitalize()
+
+
+def points_for_completed_tasks(category_name, start_date, max_points):
+    """
+    Return a function that calculates points based on the number of
+    solved tasks in a category.
+
+    Solutions are considered only if they were submitted after start_date.
+    Points are limited by max_points.
+    """
+    category = Category.objects.get(name=category_name)
+    start_date = make_aware(start_date)
+
+    def func(user):
+        points = len(category.task_set.filter(
+            solution__passed=True,
+            solution__author=user,
+            solution__submission_date__gte=start_date
+        ).distinct())
+        return min(points, max_points)
+
+    return func
+
+
+def calculate_grades(users, gradefunc):
+    """
+    Generate a sequence of 6-tuples with user data and the resulting grade using
+    the given user query set and grading function.
+    """
+    for user in users:
+        yield get_user_data(user) + (gradefunc(user), )
