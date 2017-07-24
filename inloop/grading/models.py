72cb5c79e75a1410d3361419b7b1e12c5c5e8383
Martin Morgenstern <mail@mmorgenstern.net>
2017-07-24 11:18:55 +0200

    Add models to persist plagiarism test results

---
+++ inloop/grading/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from inloop.solutions.models import Solution
+
+
+class PlagiarismTest(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    command = models.TextField(default="", help_text="Command that was used to perform the test")
+
+    def __str__(self):
+        return self.command
+
+
+class DetectedPlagiarism(models.Model):
+    test = models.ForeignKey(
+        PlagiarismTest, on_delete=models.CASCADE,
+        help_text="The test during which this plagiarism was detected"
+    )
+    solution = models.ForeignKey(
+        Solution, on_delete=models.CASCADE,
+        help_text="The solution that was found to be a rip-off"
+    )
+    veto = models.BooleanField(default=False, help_text="Cancel this detection")
+
+    def __str__(self):
+        return "solution #%d" % self.solution_id
+
+
+def ripoff_tasks_for_user(user):
+    """Return tasks for which rip-offs were detected for this user."""
+    return DetectedPlagiarism.objects.filter(
+        solution__author=user, veto=False
+    ).values_list("solution__task", flat=True).distinct()
