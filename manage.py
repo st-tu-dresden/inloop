112b77fac750faa3982663f3d7a1d4a3e6ed0376
Martin Morgenstern <mail@mmorgenstern.net>
2018-02-26 08:20:59 +0100

    Simplify Makefile, .travis.yml and manage.py with pipenv
    Fixes: #201

--- manage.py
+++ manage.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
-from environ import Env
-
 if __name__ == "__main__":
-    if "test" in sys.argv[:2]:
-        Env.read_env("tests/.env")
-    else:
-        Env.read_env(".env")
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
