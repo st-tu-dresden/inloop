7d65b20a8c6104c8b36fe26e3dd6c97cfa028d53
Martin Morgenstern <mail@mmorgenstern.net>
2018-03-04 20:06:44 +0100

    Add management commands for gitload

--- inloop/gitload/urls.py
+++ inloop/gitload/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from inloop.gitload.views import webhook_handler
 
-app_name = "github"
+app_name = "gitload"
 urlpatterns = [
     url(r'^webhook_handler/$', webhook_handler, name='webhook_handler')
 ]
