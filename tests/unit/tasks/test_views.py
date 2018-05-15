fe3efa1d309f794040f0e03d35104c385af11d63
PhilippMatthes <github@philippmatth.es>
2018-05-15 11:05:12 +0200

    Increase IndexViewTest fingerprint

--- tests/unit/tasks/test_views.py
+++ tests/unit/tasks/test_views.py
@@ -6,11 +6,19 @@ from tests.unit.tasks.mixins import TaskData
 
 
 class IndexViewTest(SimpleAccountsData, TaskData, TestCase):
-    def test_task_visibility(self):
+    def setUp(self):
         url = reverse("tasks:category", kwargs={"slug": self.category1.slug})
         self.assertTrue(self.client.login(username="bob", password="secret"))
-        response = self.client.get(url, follow=True)
-        self.assertContains(response, self.published_task1.title)
+        self.response = self.client.get(url, follow=True)
+
+    def test_task_visibility(self):
+        self.assertContains(self.response, self.published_task1.title)
+        self.assertContains(self.response, self.published_task2.title)
+        self.assertContains(self.response, self.unpublished_task1.title)
+        self.assertContains(self.response, self.unpublished_task2.title)
+
+    def test_task_styling(self):
+        print(self.response.rendered_content)
 
 
 class TaskDetailViewTest(SimpleAccountsData, TaskData, TestCase):
