13f032d5c4078729b0200a2374623c204b98dfde
Martin Morgenstern <mail@mmorgenstern.net>
2018-07-26 15:29:27 +0200

    Bring the new admin actions into shape
    Convert the admin action functions to methods their respective
    ModelAdmin classes, and ensure proper error handling and user
    notification.
    
    Also, improve wording and fix some typos along the way.

--- inloop/grading/admin.py
+++ inloop/grading/admin.py
@@ -1,33 +1,9 @@
 from os.path import basename
 
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.http import HttpResponse
 
 from inloop.grading.models import DetectedPlagiarism, PlagiarismTest
-
-
-def download_jplag_zip(modeladmin, request, queryset):
-    """
-    Fetch the stored JPlag zip file and return it as a download.
-    """
-    if len(queryset) != 1:
-        modeladmin.message_user(
-            request, "You cannot download more or less than one zip file at a time."
-        )
-        return
-    zip_file = queryset[0].zip_file
-    if not zip_file:
-        modeladmin.message_user(
-            request, "The selected plagiarism test has no downloadable zip file."
-        )
-    with open(zip_file.path, "rb") as source:
-        response = HttpResponse(source, content_type="application/zip")
-    attachment = "attachment; filename=%s" % basename(zip_file.path)
-    response["Content-Disposition"] = attachment
-    return response
-
-
-download_jplag_zip.short_description = "Download JPlag output as zip"
 
 
 class PlagiarismAdmin(admin.ModelAdmin):
@@ -52,10 +28,31 @@ class PlagiarismAdmin(admin.ModelAdmin):
 
 @admin.register(DetectedPlagiarism)
 class DetectedPlagiarismsAdmin(PlagiarismAdmin):
-    list_display = ('test', 'veto')
+    list_display = ['test', 'veto']
 
 
 @admin.register(PlagiarismTest)
 class PlagiarismTestsAdmin(PlagiarismAdmin):
-    list_display = ('id', 'created_at', 'command', 'all_detected_plagiarisms')
-    actions = (download_jplag_zip,)
+    list_display = ['id', 'created_at', 'command', 'all_detected_plagiarisms']
+    actions = ['download_jplag_zip']
+
+    def download_jplag_zip(self, request, queryset):
+        """
+        Fetch the stored JPlag zip file and return it as a download.
+        """
+        if len(queryset) != 1:
+            msg = "You can only download one zip file at a time."
+            self.message_user(request, msg, messages.WARNING)
+            return None
+        zip_file = queryset[0].zip_file
+        if not zip_file:
+            msg = "The selected test has no downloadable zip file."
+            self.message_user(request, msg, messages.WARNING)
+            return None
+        with open(zip_file.path, "rb") as source:
+            response = HttpResponse(source, content_type="application/zip")
+        attachment = "attachment; filename=%s" % basename(zip_file.path)
+        response["Content-Disposition"] = attachment
+        return response
+
+    download_jplag_zip.short_description = "Download JPlag output as zip"
--- inloop/tasks/admin.py
+++ inloop/tasks/admin.py
@@ -1,33 +1,11 @@
-from django.contrib import admin
-from django.contrib.auth.models import User
+from django.contrib import admin, messages
+from django.contrib.auth import get_user_model
 
 from inloop.grading.admin import PlagiarismAdmin
 from inloop.grading.copypasta import jplag_check_async
 from inloop.tasks.models import Category, Task
 
-
-def jplag_check_tasks(modeladmin, request, queryset):
-    """
-    Synchronous function to check selected tasks in admin model with JPlag.
-    """
-    users = User.objects.all()
-    jplag_check_async(users=users, tasks=queryset)
-    modeladmin.message_user(
-        request, "The jplag check has been dispatched. Please check again later."
-    )
-
-
-def jplag_check_category(modeladmin, request, queryset):
-    """
-    Synchronous convenience function to check selected categories in the
-    admin model with JPlag.
-    """
-    tasks = Task.objects.filter(category__in=queryset)
-    jplag_check_tasks(modeladmin, request, tasks)
-
-
-jplag_check_tasks.short_description = 'Check tasks for plagiarism'
-jplag_check_category.short_description = 'Check category for plagiarism'
+User = get_user_model()
 
 
 @admin.register(Task)
@@ -42,7 +20,17 @@ class TaskAdmin(PlagiarismAdmin):
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
 
-    actions = [jplag_check_tasks]
+    actions = ['jplag_check_tasks']
+
+    def jplag_check_tasks(self, request, queryset):
+        """
+        Admin action which starts a plagiarism check on the selected tasks.
+        """
+        jplag_check_async(users=User.objects.all(), tasks=queryset)
+        msg = "The JPlag check has been started, please check the PlagiarismTests page."
+        self.message_user(request, msg, messages.SUCCESS)
+
+    jplag_check_tasks.short_description = 'Check selected tasks for plagiarism'
 
 
 @admin.register(Category)
@@ -52,4 +40,15 @@ class CategoryAdmin(PlagiarismAdmin):
     search_fields = ['name', 'display_order']
     prepopulated_fields = {'slug': ('name',)}
 
-    actions = [jplag_check_category]
+    actions = ['jplag_check_category']
+
+    def jplag_check_category(self, request, queryset):
+        """
+        Admin action which starts a plagiarism check on the selected categories.
+        """
+        tasks = Task.objects.filter(category__in=queryset)
+        jplag_check_async(users=User.objects.all(), tasks=tasks)
+        msg = "The JPlag check has been started, please check the PlagiarismTests page."
+        self.message_user(request, msg, messages.SUCCESS)
+
+    jplag_check_category.short_description = 'Check selected categories for plagiarism'
