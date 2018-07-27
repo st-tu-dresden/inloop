855adff0c84aa6401ec67b03f49b7c52a5f6129c
Martin Morgenstern <mail@mmorgenstern.net>
2018-07-27 14:30:15 +0200

    Fix sample data formatting using yapf

--- tests/solutions/prettyprint/test_checkstyle.py
+++ tests/solutions/prettyprint/test_checkstyle.py
@@ -10,66 +10,67 @@ from . import SAMPLES_PATH
 with SAMPLES_PATH.joinpath("checkstyle.xml").open() as fp:
     CHECKSTYLE_SAMPLE_XML = fp.read()
 
-CHECKSTYLE_SAMPLE_DATA = [
-    {'version': '8.9'},
-    {
-        'tag': 'checkstyle',
-        'attrib': {'version': '8.9'},
-        'text': '\n',
-        'children': [
-            {
-                'tag': 'file',
-                'attrib': {'name': '/checker/input/Book.java'},
-                'text': '\n',
-                'children': [
-                    {
-                        'tag': 'error',
-                        'attrib': {
-                            'line': '0',
-                            'severity': 'error',
-                            'message': 'File does not end with a newline.',
-                            'source': 'checks.NewlineAtEndOfFileCheck'
-                        },
-                        'text': None,
-                        'children': []
-                    },
-                    {
-                        'tag': 'error',
-                        'attrib': {
-                            'line': '1',
-                            'column': '9',
-                            'severity': 'warning',
-                            'message': "Name 'U01.src' must match pattern"
-                                      " '^[a-z]+(\\.[a-z][a-z0-9]{1,})*$'.",
-                            'source': 'checks.naming.PackageNameCheck'
-                        },
-                        'text': None,
-                        'children': []
-                    },
-                    {
-                        'tag': 'error',
-                        'attrib': {
-                            'line': '1',
-                            'column': '17',
-                            'severity': 'error',
-                            'message': "';' is not followed by whitespace.",
-                            'source': 'checks.whitespace.WhitespaceAfterCheck'},
-                        'text': None,
-                        'children': []
-                    }
-                ]
-            }
-        ]
-    }
-]
+CHECKSTYLE_SAMPLE_DATA = [{
+    'version': '8.9'
+}, {
+    'tag':
+    'checkstyle',
+    'attrib': {
+        'version': '8.9'
+    },
+    'text':
+    '\n',
+    'children': [{
+        'tag':
+        'file',
+        'attrib': {
+            'name': '/checker/input/Book.java'
+        },
+        'text':
+        '\n',
+        'children': [{
+            'tag': 'error',
+            'attrib': {
+                'line': '0',
+                'severity': 'error',
+                'message': 'File does not end with a newline.',
+                'source': 'checks.NewlineAtEndOfFileCheck'
+            },
+            'text': None,
+            'children': []
+        }, {
+            'tag': 'error',
+            'attrib': {
+                'line': '1',
+                'column': '9',
+                'severity': 'warning',
+                'message': "Name 'U01.src' must match pattern"
+                " '^[a-z]+(\\.[a-z][a-z0-9]{1,})*$'.",
+                'source': 'checks.naming.PackageNameCheck'
+            },
+            'text': None,
+            'children': []
+        }, {
+            'tag': 'error',
+            'attrib': {
+                'line': '1',
+                'column': '17',
+                'severity': 'error',
+                'message': "';' is not followed by whitespace.",
+                'source': 'checks.whitespace.WhitespaceAfterCheck'
+            },
+            'text': None,
+            'children': []
+        }]
+    }]
+}]
 
 
 class CheckstyleXMLTests(TestCase):
     def setUp(self):
         super().setUp()
         self.xml_strings_context = context_from_xml_strings(
-            xml_strings=[CHECKSTYLE_SAMPLE_XML], filter_keys=[]
-        )
+            xml_strings=[CHECKSTYLE_SAMPLE_XML], filter_keys=[])
 
     def test_extract(self):
         data = extract_items_by_key(data=CHECKSTYLE_SAMPLE_DATA, key="file")
@@ -79,14 +80,8 @@ class CheckstyleXMLTests(TestCase):
 
     def test_to_dict(self):
         element_tree = ElementTree.fromstring(CHECKSTYLE_SAMPLE_XML)
-        dictionary1 = element_tree_to_dict(
-            element_tree,
-            filter_keys=[]
-        )
-        dictionary2 = element_tree_to_dict(
-            element_tree,
-            filter_keys=None
-        )
+        dictionary1 = element_tree_to_dict(element_tree, filter_keys=[])
+        dictionary2 = element_tree_to_dict(element_tree, filter_keys=None)
         for dictionary in [dictionary1, dictionary2]:
             self.assertTrue(isinstance(dictionary, dict))
             self.assertEqual(dictionary["tag"], "checkstyle")
