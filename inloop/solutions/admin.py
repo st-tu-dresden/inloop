1de83dd3adb278ef2c1b72c3a89fe88c5593e427
PhilippMatthes <github@philippmatth.es>
2018-11-18 17:53:34 +0100

    Add semester-based filters to admin solution view
    Pull Request: #260
    Fixes: #230

--- inloop/solutions/admin.py
+++ inloop/solutions/admin.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timedelta, timezone
+
 from django.contrib import admin
+from django.utils.timezone import now
 
 from inloop.solutions.models import Solution, SolutionFile
 from inloop.solutions.statistics import Statistics
@@ -10,6 +13,36 @@ class SolutionFileInline(admin.StackedInline):
     max_num = 0
 
 
+class SemesterFieldListFilter(admin.DateFieldListFilter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title = "semester"
+        self.links = self.generate_links()
+
+    def generate_links(self):
+        """Derive semesters of submitted solutions."""
+        semesters = [('Any semester', {})]
+        first_solution = Solution.objects.first()
+        if not first_solution:
+            return semesters
+
+        # start and end are both in the CEST timezone
+        tzinfo_cest = timezone(timedelta(hours=2))
+        summer_start = datetime(2018, 4, 1, tzinfo=tzinfo_cest)
+        winter_start = datetime(2018, 10, 1, tzinfo=tzinfo_cest)
+
+        for year in range(first_solution.submission_date.year, now().year + 1):
+            semesters.append(("Summer {}".format(year), {
+                self.lookup_kwarg_since: str(summer_start.replace(year)),
+                self.lookup_kwarg_until: str(winter_start.replace(year))
+            }))
+            semesters.append(("Winter {}/{}".format(year, year + 1), {
+                self.lookup_kwarg_since: str(winter_start.replace(year)),
+                self.lookup_kwarg_until: str(summer_start.replace(year=year + 1))
+            }))
+        return semesters
+
+
 @admin.register(Solution)
 class SolutionAdmin(admin.ModelAdmin):
     class Media:
@@ -19,7 +52,13 @@ class SolutionAdmin(admin.ModelAdmin):
     change_list_template = "admin/solutions/solutions.html"
     inlines = [SolutionFileInline]
     list_display = ['id', 'author', 'task', 'submission_date', 'passed', 'site_link']
-    list_filter = ['passed', 'task__category', 'submission_date', 'task']
+    list_filter = [
+        'passed',
+        'task__category',
+        ('submission_date', SemesterFieldListFilter),
+        'submission_date',
+        'task'
+    ]
     search_fields = [
         'author__username', 'author__email', 'author__first_name', 'author__last_name'
     ]
@@ -29,7 +68,7 @@ class SolutionAdmin(admin.ModelAdmin):
         return '<a href="%s">%s details</a>' % (obj.get_absolute_url(), obj)
 
     def changelist_view(self, request, extra_context=None):
-        # Filter our admin view by the selected viewport filter
+        """Filter admin view by the selected viewport filter."""
         solutions = Solution.objects.filter(**request.GET.dict())
         extra = {}
         if solutions:
