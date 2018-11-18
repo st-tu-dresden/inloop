d208a62b84a79c822b178f3007a2e074774b0d4c
PhilippMatthes <github@philippmatth.es>
2018-11-18 17:53:34 +0100

    Add semester/month filter for admin solution view

--- inloop/solutions/admin.py
+++ inloop/solutions/admin.py
@@ -1,13 +1,95 @@
+import calendar
+
 from django.contrib import admin
+from django.utils import timezone
 
 from inloop.solutions.models import Solution, SolutionFile
 from inloop.solutions.statistics import Statistics
+
+
+def start_of_summer_semester(year):
+    return str(timezone.localdate(timezone.now()).replace(year=year, month=4, day=1))
+
+
+def start_of_winter_semester(year):
+    return str(timezone.localdate(timezone.now()).replace(year=year, month=10, day=1))
+
+
+def start_of_month(year, month):
+    return str(timezone.localdate(timezone.now()).replace(year=year, month=month, day=1))
+
+
+def end_of_month(year, month):
+    month = month % 12 + 1
+    return str(timezone.localdate(timezone.now()).replace(year=year, month=month, day=1))
 
 
 class SolutionFileInline(admin.StackedInline):
     model = SolutionFile
     readonly_fields = ['file']
     max_num = 0
+
+
+class SemesterFieldListFilter(admin.DateFieldListFilter):
+    def __init__(self, *args, **kwargs):
+        super(SemesterFieldListFilter, self).__init__(*args, **kwargs)
+        self.title = "semester"
+        self.links = self.generate_links()
+
+    def generate_links(self):
+        """Derive semesters of submitted solutions."""
+        solutions = Solution.objects.all().order_by("submission_date")
+        if not solutions:
+            return None
+        first_solution_submission_date = solutions[0].submission_date.date()
+        first_solution_year = first_solution_submission_date.year
+        current_year = timezone.now().year
+        semesters = []
+
+        for year in range(first_solution_year, current_year + 1):
+            semesters.append(
+                ("Summer Semester {}".format(year), {
+                    self.lookup_kwarg_since: start_of_summer_semester(year),
+                    self.lookup_kwarg_until: start_of_winter_semester(year)
+                })
+            )
+
+            semesters.append(
+                ("Winter Semester {}".format(year), {
+                    self.lookup_kwarg_since: start_of_winter_semester(year),
+                    self.lookup_kwarg_until: start_of_summer_semester(year + 1)
+                })
+            )
+
+        return semesters
+
+
+class LastMonthsDateFieldFilter(admin.DateFieldListFilter):
+    def __init__(self, *args, **kwargs):
+        super(LastMonthsDateFieldFilter, self).__init__(*args, **kwargs)
+        self.title = "month"
+        self.links = list(self.links)
+        self.links += self.generate_links()
+
+    @staticmethod
+    def get_last_months():
+        solutions = Solution.objects.all()
+        months = set()
+        for solution in solutions:
+            months.add((solution.submission_date.year, solution.submission_date.month))
+        return months
+
+    def generate_links(self):
+        """Derive last 12 months."""
+        months = []
+        for (year, month) in LastMonthsDateFieldFilter.get_last_months():
+            months.append(
+                ("{} {}".format(calendar.month_abbr[month], year), {
+                    self.lookup_kwarg_since: start_of_month(year, month),
+                    self.lookup_kwarg_until: end_of_month(year, month)
+                })
+            )
+        return months
 
 
 @admin.register(Solution)
@@ -19,7 +101,13 @@ class SolutionAdmin(admin.ModelAdmin):
     change_list_template = "admin/solutions/solutions.html"
     inlines = [SolutionFileInline]
     list_display = ['id', 'author', 'task', 'submission_date', 'passed', 'site_link']
-    list_filter = ['passed', 'task__category', 'submission_date', 'task']
+    list_filter = [
+        'passed',
+        'task__category',
+        ('submission_date', SemesterFieldListFilter),
+        ('submission_date', LastMonthsDateFieldFilter),
+        'task'
+    ]
     search_fields = [
         'author__username', 'author__email', 'author__first_name', 'author__last_name'
     ]
@@ -29,7 +117,7 @@ class SolutionAdmin(admin.ModelAdmin):
         return '<a href="%s">%s details</a>' % (obj.get_absolute_url(), obj)
 
     def changelist_view(self, request, extra_context=None):
-        # Filter our admin view by the selected viewport filter
+        """Filter admin view by the selected viewport filter"""
         solutions = Solution.objects.filter(**request.GET.dict())
         extra = {}
         if solutions:
