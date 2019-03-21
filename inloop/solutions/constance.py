bfb3b90b818b9582c1f35d70af350d808bd5efe6
Martin Morgenstern <martin.morgenstern1@tu-dresden.de>
2019-03-21 16:43:17 +0100

    Adjust spacing and wording of the help text

--- inloop/solutions/constance.py
+++ inloop/solutions/constance.py
@@ -6,9 +6,9 @@ config = OrderedDict()
 config["ALLOWED_FILENAME_EXTENSIONS"] = (".java", mark_safe(
     "Filename extensions to be accepted in solution upload forms "
     "such as <code>.java, .c, .cpp, .h, .hpp</code>. "
-    "Separate multiple filename extensions by commas."
-    "Surrounding whitespace will be stripped."
-    "Filename extensions will be treated case insensitive."
+    "Separate multiple filename extensions by commas. "
+    "Surrounding whitespace will be stripped and "
+    "filename extensions will be treated case insensitive."
 ))
 
 fieldsets = {"General settings": ["ALLOWED_FILENAME_EXTENSIONS"]}
