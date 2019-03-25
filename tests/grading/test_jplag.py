09210af24c034cade61bb63d6d8a75ec85c60f00
PhilippMatthes <github@philippmatth.es>
2019-03-25 13:30:37 +0100

    Add plagiarism variation detection test

--- tests/grading/test_jplag.py
+++ tests/grading/test_jplag.py
@@ -11,8 +11,9 @@ from inloop.solutions.models import SolutionFile
 
 from tests.grading.mixins import PlagiatedSolutionsData
 
-FIBONACCI = """
+FIBONACCI_ITERATIVE = """
 public class Fibonacci {
+    // Iterative solution
     public static int fib(final int x) {
     if (x < 0) {
         throw new IllegalArgumentException("x must be greater than or equal zero");
@@ -31,8 +32,34 @@ public class Fibonacci {
     }
 
     /*
-     * This string is just here for testing purposes.
+     * This comment is just here for testing purposes.
      */
+}
+"""
+
+FIBONACCI_ITERATIVE_SHORTENED = """
+public class Fibonacci {
+    public static int fib(final int x) {
+        if (x < 0) throw new IllegalArgumentException("x must be greater than or equal zero");
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < x; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+}
+"""
+
+FIBONACCI_RECURSIVE = """
+public class Fibonacci {
+    // Recursive solution
+    public static int fib(int x) {
+        if (x < 2) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 """
 
@@ -41,7 +68,7 @@ TEST_MEDIA_ROOT = mkdtemp()
 
 @tag("slow")
 @override_settings(MEDIA_ROOT=TEST_MEDIA_ROOT)
-class JPlagCheckTest(PlagiatedSolutionsData, TestCase):
+class JPlagSimplePlagiarismDetectionTest(PlagiatedSolutionsData, TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -52,11 +79,11 @@ class JPlagCheckTest(PlagiatedSolutionsData, TestCase):
         super().setUp()
         self.plagiated_solution_file_bob = SolutionFile.objects.create(
             solution=self.passed_solution_bob,
-            file=SimpleUploadedFile('FibonacciBob.java', FIBONACCI.encode())
+            file=SimpleUploadedFile('FibonacciBob.java', FIBONACCI_ITERATIVE.encode())
         )
         self.plagiated_solution_file_alice = SolutionFile.objects.create(
             solution=self.passed_solution_alice,
-            file=SimpleUploadedFile('FibonacciAlice.java', FIBONACCI.encode())
+            file=SimpleUploadedFile('FibonacciAlice.java', FIBONACCI_ITERATIVE.encode())
         )
 
     def test_jplag_check_with_resultdir(self):
@@ -78,6 +105,44 @@ class JPlagCheckTest(PlagiatedSolutionsData, TestCase):
             tasks=[self.task],
             min_similarity=100,
         )
+        self.assertTrue(self.passed_solution_bob in output)
+        self.assertTrue(self.passed_solution_alice in output)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(TEST_MEDIA_ROOT)
+        super().tearDownClass()
+
+
+@tag("slow")
+@override_settings(MEDIA_ROOT=TEST_MEDIA_ROOT)
+class JPlagPlagiarismVariationDetectionTest(PlagiatedSolutionsData, TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        if not os.path.isdir(TEST_MEDIA_ROOT):
+            os.makedirs(TEST_MEDIA_ROOT)
+
+    def setUp(self):
+        super().setUp()
+        self.plagiated_solution_file_bob = SolutionFile.objects.create(
+            solution=self.passed_solution_bob,
+            file=SimpleUploadedFile('FibonacciBob.java', FIBONACCI_ITERATIVE.encode())
+        )
+        self.plagiated_solution_file_alice = SolutionFile.objects.create(
+            solution=self.passed_solution_alice,
+            file=SimpleUploadedFile('FibonacciAlice.java', FIBONACCI_ITERATIVE_SHORTENED.encode())
+        )
+
+    def test_comment_variation_detection(self):
+        """Validate that simple code variations are still recognized as plagiarisms."""
+        with TemporaryDirectory() as path:
+            output = jplag_check(
+                users=[self.alice, self.bob],
+                tasks=[self.task],
+                min_similarity=100,
+                result_dir=Path(path).joinpath("jplag")
+            )
         self.assertTrue(self.passed_solution_bob in output)
         self.assertTrue(self.passed_solution_alice in output)
 
