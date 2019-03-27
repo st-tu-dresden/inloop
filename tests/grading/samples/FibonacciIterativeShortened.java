990671d87c04d8728f3289e3b5169a12bfa34051
PhilippMatthes <github@philippmatth.es>
2019-03-27 13:20:03 +0100

    Move java test samples to separate files

---
+++ tests/grading/samples/FibonacciIterativeShortened.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static int fib(final int x) {
+        if (x < 0) throw new IllegalArgumentException("x must be greater than or equal zero");
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < x; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+}
