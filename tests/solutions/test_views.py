bcf2253465cb559b07d5b16eaf2d6ce65f3f299e
PhilippMatthes <github@philippmatth.es>
2019-04-11 11:53:54 +0200

    Add solution detail view redirect test

--- tests/solutions/test_views.py
+++ tests/solutions/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from django.utils.encoding import force_text
 
 from inloop.solutions.models import Solution
@@ -24,3 +25,25 @@ class SolutionStatusViewTest(TaskData, SimpleAccountsData, TestCase):
         self.assertTrue(self.client.login(username="alice", password="secret"))
         response = self.client.get("/solutions/%d/status" % self.solution.id)
         self.assertEqual(response.status_code, 404)
+
+
+class SolutionDetailViewRedirectTest(TaskData, SimpleAccountsData, TestCase):
+    def setUp(self):
+        self.solution = Solution.objects.create(author=self.bob, task=self.published_task1)
+
+    def test_unchecked_solution_redirects(self):
+        """
+        Test that requesting a solution detail view redirects
+        to the solution list when it was not checked yet.
+        """
+        self.assertTrue(self.client.login(username="bob", password="secret"))
+        response = self.client.get(reverse("solutions:detail", kwargs={
+            "slug": self.solution.task.slug, "scoped_id": self.solution.scoped_id
+        }), follow=False)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("solutions:list", kwargs={
+            "slug": self.solution.task.slug
+        }))
+
+
+
