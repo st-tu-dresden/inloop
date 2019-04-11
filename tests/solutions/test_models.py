8dc6aabe582a1d2a603e1e2583b3c8c3dd4bf8f4
PhilippMatthes <github@philippmatth.es>
2019-04-11 11:32:14 +0200

    Add archive packaging test

--- tests/solutions/test_models.py
+++ tests/solutions/test_models.py
@@ -1,9 +1,15 @@
+import os
+import shutil
+import zipfile
+from tempfile import TemporaryDirectory, mkdtemp
 from unittest.mock import Mock
 
-from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase, override_settings
 from django.utils import timezone
 
-from inloop.solutions.models import Solution, get_upload_path
+from inloop.solutions.models import (Solution, SolutionFile, create_archive_async,
+                                     get_solution_upload_path, get_upload_path)
 
 from tests.solutions.mixins import SolutionsData
 
@@ -15,7 +21,11 @@ class SolutionsModelTest(SolutionsData, TestCase):
         self.assertFalse(self.failed_solution.passed)
 
     def test_get_upload_path(self):
-        """Test the upload path for solution files."""
+        """
+        Test the upload path for:
+            - solution files
+            - archives associated with a particular solution
+        """
 
         # Create a mock solution to check its
         # computed upload path
@@ -32,6 +42,17 @@ class SolutionsModelTest(SolutionsData, TestCase):
         tokens = upload_path.split("/")
         for token in ["1970", "here-be-dragons", "123456"]:
             self.assertTrue(token in tokens)
+
+        upload_path_associated_archive = get_solution_upload_path(
+            mock.solution,
+            "archive.zip"
+        )
+        # Validate that an associated archive is stored
+        # to the same directory as the solution files themselves
+        self.assertEqual(
+            os.path.dirname(os.path.abspath(upload_path)),
+            os.path.dirname(os.path.abspath(upload_path_associated_archive))
+        )
 
     def test_solution_default_status(self):
         """Verify that the default status of a solution is pending."""
@@ -56,3 +77,111 @@ class SolutionsModelTest(SolutionsData, TestCase):
         mocked_time = timezone.now() - timezone.timedelta(hours=1)
         delayed_solution.submission_date = mocked_time
         self.assertEqual(delayed_solution.status(), "lost")
+
+
+JAVA_EXAMPLE_1 = """
+public class Example {
+    //...
+}
+""".encode()
+
+JAVA_EXAMPLE_2 = "".encode()
+
+
+TEST_MEDIA_ROOT = mkdtemp()
+
+
+@override_settings(MEDIA_ROOT=TEST_MEDIA_ROOT)
+class SolutionsModelArchiveTest(SolutionsData, TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        if not os.path.isdir(TEST_MEDIA_ROOT):
+            os.makedirs(TEST_MEDIA_ROOT)
+
+    def setUp(self):
+        super().setUp()
+        self.solution_file_passed_1 = SolutionFile.objects.create(
+            solution=self.passed_solution,
+            file=SimpleUploadedFile("Example1.java", JAVA_EXAMPLE_1)
+        )
+        self.solution_file_passed_1_duplicate = SolutionFile.objects.create(
+            solution=self.passed_solution,
+            file=SimpleUploadedFile("Example1.java", JAVA_EXAMPLE_1)
+        )
+        self.solution_file_passed_2 = SolutionFile.objects.create(
+            solution=self.passed_solution,
+            file=SimpleUploadedFile("Example2.java", JAVA_EXAMPLE_2)
+        )
+        self.solution_file_failed_1 = SolutionFile.objects.create(
+            solution=self.failed_solution,
+            file=SimpleUploadedFile("Example1.java", JAVA_EXAMPLE_1)
+        )
+        self.solution_file_failed_2 = SolutionFile.objects.create(
+            solution=self.failed_solution,
+            file=SimpleUploadedFile("Example2.java", JAVA_EXAMPLE_2)
+        )
+        self.solution_file_failed_2_duplicate = SolutionFile.objects.create(
+            solution=self.failed_solution,
+            file=SimpleUploadedFile("Example2.java", JAVA_EXAMPLE_2)
+        )
+
+    def test_archive_creation(self):
+        """Test archive creation for solutions."""
+        for solution in [self.passed_solution, self.failed_solution]:
+            self.assertFalse(solution.archive)
+
+            create_archive_async(solution)
+
+            self.assertTrue(solution.archive)
+            self.assertTrue(zipfile.is_zipfile(solution.archive.path))
+            with zipfile.ZipFile(solution.archive.path) as zip_file:
+                self.assertIn("Example1.java", zip_file.namelist())
+                self.assertIn("Example2.java", zip_file.namelist())
+
+                # Duplicate files should be packed as well and renamed beforehand
+                self.assertEqual(3, len(zip_file.namelist()))
+                duplicate_filenames = [
+                    n for n in zip_file.namelist()
+                    if n != "Example1.java" and n != "Example2.java"
+                ]
+                self.assertEqual(1, len(duplicate_filenames))
+                duplicate_filename = duplicate_filenames[0]
+                self.assertTrue(
+                    "Example1" in duplicate_filename or "Example2" in duplicate_filename
+                )
+
+                with zip_file.open("Example1.java") as f:
+                    self.assertEqual(f.read(), JAVA_EXAMPLE_1)
+                with zip_file.open("Example2.java") as f:
+                    self.assertEqual(f.read(), JAVA_EXAMPLE_2)
+                with zip_file.open(duplicate_filename) as f:
+                    contents = f.read()
+                    self.assertTrue(
+                        contents == JAVA_EXAMPLE_1 or contents == JAVA_EXAMPLE_2
+                    )
+
+                with TemporaryDirectory() as tmp_dir:
+                    # Test extraction to expose erroneous packaging
+                    zip_file.extractall(tmp_dir)
+                    for _, dirs, files in os.walk(tmp_dir):
+                        self.assertIn("Example1.java", files)
+                        self.assertIn("Example2.java", files)
+                        self.assertIn(duplicate_filename, files)
+                        self.assertEqual(3, len(files))
+
+                        # The inherited folder structure should be flat
+                        self.assertFalse(dirs)
+
+                self.assertIsNone(zip_file.testzip())
+
+                # Validate that the generated archive has a descriptive name
+                self.assertIn(self.task.underscored_title, zip_file.filename)
+                self.assertIn(str(solution.scoped_id), zip_file.filename)
+                self.assertIn(".zip", zip_file.filename)
+                self.assertIn("Solution", zip_file.filename)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(TEST_MEDIA_ROOT)
+        super().tearDownClass()
