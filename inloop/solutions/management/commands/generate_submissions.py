fdc84a0f1e3f8367b800b05c75ce4123736f68f6
Martin Morgenstern <martin.morgenstern1@tu-dresden.de>
2020-01-19 10:29:07 +0100

    Rewrite the generate_submissions command
    * Ensure increasing submission_date for increasing solution_ids
    * Remove the --force option
    * Restrict usage to DEBUG mode
    * Add --success_rate option
    * Convert required options to positional arguments
    
    Fixes: #331
    Resolves: #332

--- inloop/solutions/management/commands/generate_submissions.py
+++ inloop/solutions/management/commands/generate_submissions.py
@@ -1,106 +1,91 @@
+import functools
 import random
-from datetime import datetime, timedelta
-from pathlib import Path
+from datetime import timedelta
 
+from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.db.transaction import atomic
-from django.utils.timezone import make_aware
+from django.utils import timezone
 
 from inloop.solutions.models import Solution
 from inloop.tasks.models import Task
-
-User = get_user_model()
-
-file_dir = Path(__file__).resolve().parent
 
 
 class Command(BaseCommand):
     help = 'Generate submissions for demo purposes.'
 
-    dateformat = '%d.%m.%Y'
-    help_text_safe_dateformat = 'dd.mm.YYYY'
-
-    def create_users(self, number):
-        users = []
-        for i in range(number):
-            username = f'GeneratedUser{i}'
-            email = f'generated-user-{i}@example.com'
-            password = 'secret'
-            user, _ = User.objects.get_or_create(
-                username=username, email=email, password=password
-            )
-            users.append(user)
-        return users
-
-    def create_solution(self, user, task, start_date, end_date):
-        passed = random.choice([True, False])
-        seconds_until_end_date = int((end_date - start_date).total_seconds())
-        submission_date = start_date + timedelta(
-            seconds=random.randint(0, seconds_until_end_date)
-        )
-        solution = Solution.objects.create(
-            author=user, task=task, passed=passed,
-        )
-        # Because of auto_now_add, we can't set a custom
-        # submission_date through the constructor
-        solution.submission_date = submission_date
-        solution.save()
-        return solution
+    MAX_USERS = 10**4
+    AVG_SOLUTIONS_PER_HOUR = 0.5
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--start_date',
-            help=f'Start date (Format: {self.help_text_safe_dateformat})',
-            default=(datetime.now() - timedelta(days=365)).strftime(self.dateformat),
+            '--success_rate',
+            help='Ratio of solutions to mark as passed (e.g., 0.25)',
+            default=0.25,
+            type=float
         )
         parser.add_argument(
-            '--end_date',
-            help=f'End date (Format: {self.help_text_safe_dateformat})',
-            default=datetime.now().strftime(self.dateformat),
-        )
-        parser.add_argument(
-            '--solutions_number',
-            help='Number of solutions to generate',
-            type=int,
-            default=1000
-        )
-        parser.add_argument(
-            '--users_number',
+            'num_users',
             help='Number of users to generate',
-            type=int,
-            default=10
+            type=int
         )
         parser.add_argument(
-            '--force',
-            help='Immediately perform submission creation without prompt',
-            default=False
+            'num_solutions',
+            help='Number of solutions to generate',
+            type=int
         )
 
     def handle(self, *args, **options):
-        start_date = make_aware(datetime.strptime(options['start_date'], self.dateformat))
-        end_date = make_aware(datetime.strptime(options['end_date'], self.dateformat))
-        solutions_number = options['solutions_number']
-        users_number = options['users_number']
-        verbosity = options['verbosity']
-        force = options['force']
-        if start_date > end_date:
-            raise ValueError('Start date should be before end date.')
-        if not Task.objects.exists():
-            raise ValueError('There are no tasks available.')
-        if not force:
-            if input('This will create user accounts and solutions. Continue? (y/n)') != 'y':
-                return
+        success_rate = options['success_rate']
+        num_solutions = options['num_solutions']
+        num_users = options['num_users']
+        if not settings.DEBUG:
+            raise CommandError('This command should only be used in DEBUG mode.')
+        if num_users < 1 or num_solutions < 1:
+            raise CommandError('num_users and num_solutions must be >= 1.')
+        if num_users > self.MAX_USERS:
+            raise CommandError(f'num_users must be <= {self.MAX_USERS}.')
+        if not (0.0 <= success_rate <= 1.0):
+            raise CommandError('success_rate must be within range [0.0, 1.0].')
         with atomic():
-            users = self.create_users(users_number)
-            tasks = Task.objects.all()
-            all_solutions = []
-            for _ in range(solutions_number):
-                user = random.choice(users)
-                task = random.choice(tasks)
-                all_solutions.append(self.create_solution(
-                    user, task, start_date, end_date
-                ))
-        if verbosity > 0:
-            self.stdout.write(f'Successfully created {len(users)} users.')
-            self.stdout.write(f'Successfully created {len(all_solutions)} solutions.')
+            self.generate_submissions(num_users, num_solutions, success_rate)
+        self.stdout.write(f'Successfully created {num_users} user(s).')
+        self.stdout.write(f'Successfully created {num_solutions} solution(s).')
+
+    def generate_submissions(self, num_users, num_solutions, success_rate):
+        if Solution.objects.exists():
+            raise CommandError('The solutions table must be empty.')
+        tasks = Task.objects.all()
+        if not tasks:
+            raise CommandError('There are no tasks available.')
+        now = timezone.now()
+        start_date = now.replace(year=2015, month=4, day=15)  # birthday of INLOOP (see #14)
+        users = self.generate_users(num_users, date_joined=start_date)
+        for n in range(num_solutions):
+            if start_date > now:
+                # reject creating unrealistic data
+                raise CommandError(f'Could only create {n} of {num_solutions} solutions.')
+            solution = Solution.objects.create(
+                author=random.choice(users),
+                task=random.choice(tasks),
+                passed=(random.random() < success_rate)
+            )
+            # because of auto_now_add, we can't set a custom
+            # submission_date through the constructor
+            solution.submission_date = start_date
+            solution.save()
+            seconds_to_add = random.randint(0, int(2 * 3600 / self.AVG_SOLUTIONS_PER_HOUR))
+            start_date += timedelta(seconds=seconds_to_add)
+
+    def generate_users(self, num_users, *, date_joined):
+        usernames = [f'student{n:04d}' for n in random.sample(range(self.MAX_USERS), num_users)]
+        create_user = functools.partial(
+            get_user_model().objects.get_or_create,
+            date_joined=date_joined,
+            password='secret'
+        )
+        return [
+            create_user(username=username, email=f'{username}@localhost')[0]
+            for username in usernames
+        ]
--- tests/solutions/test_commands.py
+++ tests/solutions/test_commands.py
@@ -1,13 +1,11 @@
-from datetime import datetime
 from io import StringIO
 from unittest.mock import patch
 
 from django.contrib.auth import get_user_model
-from django.core.management import call_command
-from django.test import TestCase, tag
-from django.utils.timezone import make_aware
+from django.core.management import CommandError, call_command
+from django.test import TestCase, override_settings
+from django.utils import timezone
 
-from inloop.solutions.management.commands.generate_submissions import Command
 from inloop.solutions.models import Solution
 from inloop.tasks.models import Task
 
@@ -15,105 +13,54 @@ from tests.solutions.mixins import SimpleTaskData
 
 User = get_user_model()
 
-DATEFORMAT = Command.dateformat
-START_DATE_STR = '01.01.1970'
-END_DATE_STR = '31.12.2018'
 
+@override_settings(DEBUG=True)
+class GenerateSubmissionsCommandTest(SimpleTaskData, TestCase):
+    @override_settings(DEBUG=False)
+    def test_generate_requires_debugmode(self):
+        with self.assertRaisesRegex(CommandError, 'only be used in DEBUG'):
+            call_command('generate_submissions', '2', '5')
 
-@tag('slow')
-class GenerateSubmissionsTest(SimpleTaskData, TestCase):
-    def test_command(self):
-        """Verify that submissions can be generated correctly."""
-        opts = {
-            'start_date': START_DATE_STR,
-            'end_date': END_DATE_STR,
-            'solutions_number': 10,
-            'users_number': 10,
-            'force': True,
-            'stdout': StringIO()
-        }
-        call_command('generate_submissions', **opts)
+    def test_params_are_validated1(self):
+        with self.assertRaisesRegex(CommandError, 'must be >= 1'):
+            call_command('generate_submissions', '0', '5')
+        with self.assertRaisesRegex(CommandError, 'must be >= 1'):
+            call_command('generate_submissions', '2', '0')
 
-        self.assertEqual(Solution.objects.all().count(), 10)
-        self.assertEqual(User.objects.all().count(), 10)
+    def test_params_are_validated2(self):
+        with self.assertRaisesRegex(CommandError, 'success_rate'):
+            call_command('generate_submissions', '2', '5', success_rate=-0.1)
+        with self.assertRaisesRegex(CommandError, 'success_rate'):
+            call_command('generate_submissions', '2', '5', success_rate=1.1)
 
-        start_date = make_aware(datetime.strptime(START_DATE_STR, DATEFORMAT))
-        end_date = make_aware(datetime.strptime(END_DATE_STR, DATEFORMAT))
+    def test_params_are_validated3(self):
+        with self.assertRaisesRegex(CommandError, 'num_users must be <= 10000'):
+            call_command('generate_submissions', '10001', '5')
 
-        for solution in Solution.objects.all():
-            self.assertTrue(solution.submission_date > start_date)
-            self.assertTrue(solution.submission_date < end_date)
+    def test_generate_requires_empty_solutions(self):
+        bob = User.objects.create(username='bob', email='bob@localhost', password='secret')
+        Solution.objects.create(author=bob, task=self.task, passed=False)
+        with self.assertRaisesRegex(CommandError, 'solutions table must be empty'):
+            call_command('generate_submissions', '2', '5')
 
-    def test_force(self):
-        """Test the force flag of the management command."""
-        opts = {
-            'start_date': START_DATE_STR,
-            'end_date': END_DATE_STR,
-            'force': True,
-            'stdout': StringIO()
-        }
-        call_command('generate_submissions', **opts)
-        self.assertNotEqual(Solution.objects.all().count(), 0)
-        self.assertNotEqual(User.objects.all().count(), 0)
+    def test_generate_requires_tasks(self):
+        Task.objects.all().delete()
+        with self.assertRaisesRegex(CommandError, 'no tasks available'):
+            call_command('generate_submissions', '2', '5')
 
-    def test_prompt(self):
-        """
-        Verify that the user has to confirm the
-        generation of submissions first.
-        """
-        opts = {
-            'start_date': START_DATE_STR,
-            'end_date': END_DATE_STR,
-            'stdout': StringIO()
-        }
-        func_to_patch = 'inloop.solutions.management.commands.generate_submissions.input'
-        with patch(func_to_patch, return_value='n'):
-            call_command('generate_submissions', **opts)
-        self.assertEqual(Solution.objects.all().count(), 0)
-        self.assertEqual(User.objects.all().count(), 0)
+    def test_solutions_and_users_are_generated(self):
+        stdout = StringIO()
+        call_command('generate_submissions', '2', '5', stdout=stdout)
+        self.assertIn('created 2 user(s)', stdout.getvalue())
+        self.assertIn('created 5 solution(s)', stdout.getvalue())
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(Solution.objects.count(), 5)
+        solutions = list(Solution.objects.all())
+        for s1, s2 in zip(solutions, solutions[1:]):
+            self.assertLess(s1.submission_date, s2.submission_date)
 
-    def test_erronenous_input_dates(self):
-        """Verify that the input dates are checked for validity."""
-        opts = {
-            'start_date': END_DATE_STR,
-            'end_date': START_DATE_STR,
-            'stdout': StringIO()
-        }
-        try:
-            call_command('generate_submissions', **opts)
-            self.fail('The management command should check the input dates for validity.')
-        except ValueError:
-            pass
-
-    def test_silent(self):
-        """Verify that command line output is only produced when needed."""
-        out = StringIO()
-        opts = {
-            'start_date': START_DATE_STR,
-            'end_date': END_DATE_STR,
-            'force': True,
-            'stdout': out,
-            'verbosity': 0
-        }
-        call_command('generate_submissions', **opts)
-        self.assertFalse(out.getvalue())
-        opts['verbosity'] = 1
-        call_command('generate_submissions', **opts)
-        self.assertTrue(out.getvalue())
-
-
-class EmptyDatabaseGenerateTest(TestCase):
-    def test_no_task(self):
-        """Verify that the command fails when there is no available task."""
-        opts = {
-            'start_date': START_DATE_STR,
-            'end_date': END_DATE_STR,
-            'force': True,
-            'stdout': StringIO()
-        }
-        self.assertEqual(Task.objects.count(), 0)
-        try:
-            call_command('generate_submissions', **opts)
-            self.fail('The management command should fail, when there are no tasks.')
-        except ValueError:
-            pass
+    def test_error_raised_for_unrealistic_data(self):
+        with patch('inloop.solutions.management.commands.generate_submissions.timezone') as mock:
+            mock.now.return_value = timezone.now().replace(year=2014)
+            with self.assertRaisesRegexp(CommandError, '0 of 5 solutions'):
+                call_command('generate_submissions', '2', '5')
