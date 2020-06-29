3c338c70b4cff8ab154e40afbc267c243968b398
Martin Morgenstern <mail@mmorgenstern.net>
2020-06-29 19:12:59 +0200

    Remove obsolete scripts

--- csslint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CSS_FILES=$(find ./assets -type f -name "*.css" -not -name "*.min.css" -not -path "*/vendor/*")
-for f in ${CSS_FILES}; do
-    echo "Checking ${f}"
-    csslint --format=compact ${f}
-done
