b63b2cd670de5b8e0e8bdcbfaeaf226735020c60
Martin Morgenstern <mail@mmorgenstern.net>
2020-08-10 13:22:06 +0200

    Sync the hardcoded status url with the current urlconf

--- assets/js/solution-status.js
+++ assets/js/solution-status.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(".timeago").timeago();
   $(".solution-pending").refreshJSON("activate", {
-    url: "/solutions/solution/status/:id:/",
+    url: "/solutions/status/:id:/",
     interval: 5000,
     success: function(data) {
       $(this).attr("class", "glyphicon solution-" + data.status);
