57c55f46b54b9eebc83c80e4b2167f643d08db88
Julian Catoni <julian.catoni@tu-dresden.de>
2020-08-10 11:43:20 +0200

    Check whether upload exists before accessing it
    Add check, whether the upload input element really exists before attaching an event listener to it. Needed when a deadline is expired.
    
    Fixes: #376

--- assets/js/editor/editor.js
+++ assets/js/editor/editor.js
@@ -730,7 +730,8 @@ class Toolbar {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById(MANUAL_UPLOAD_INPUT_ID).addEventListener("change", function () {
+  const uploadInputElement = document.getElementById(MANUAL_UPLOAD_INPUT_ID);
+  uploadInputElement && uploadInputElement.addEventListener("change", function () {
     document.getElementById(MANUAL_UPLOAD_FORM_ID).submit();
   });
 
