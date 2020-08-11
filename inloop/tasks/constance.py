b70432be289a7124003adcb6d577ba1a2c6a7497
Martin Morgenstern <mail@mmorgenstern.net>
2020-08-11 23:52:00 +0200

    Update outdated config help text

--- inloop/tasks/constance.py
+++ inloop/tasks/constance.py
@@ -3,8 +3,7 @@ from django.utils.safestring import mark_safe
 config = {
     'MAX_SUBMISSIONS': (-1, mark_safe(
         'Submission limit per task and user. The value <code>-1</code> means '
-        'unlimited. Tasks can override this setting individually. Furthermore, '
-        'these limits do not apply to admin and staff users.'
+        'unlimited. Tasks can override this setting individually.'
     ))
 }
 fieldsets = {
