d9765bf5339d03378bb46eca586d77e6ef612622
Martin Morgenstern <mail@mmorgenstern.net>
2020-08-11 10:01:29 +0200

    Add properties for submission limits to the Task model

--- tests/tasks/test_models.py
+++ tests/tasks/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.utils import timezone
 
+from constance.test import override_config
+
 from inloop.solutions.models import Solution
 from inloop.tasks.models import Category, Task
 
@@ -31,6 +33,55 @@ class TaskTests(SimpleAccountsData, TaskData, TestCase):
         Solution.objects.create(author=self.bob, task=self.published_task1, passed=True)
         self.assertTrue(self.published_task1.is_completed_by(self.bob))
         self.assertFalse(self.published_task2.is_completed_by(self.bob))
+
+    def test_default_submission_limit(self):
+        self.assertIsNone(self.published_task1.max_submissions)
+        self.assertEqual(self.published_task1.submission_limit, -1)
+        self.assertFalse(self.published_task1.has_submission_limit)
+
+    @override_config(MAX_SUBMISSIONS=10)
+    def test_custom_submission_limit(self):
+        self.assertIsNone(self.published_task1.max_submissions)
+        self.assertEqual(self.published_task1.submission_limit, 10)
+        self.assertTrue(self.published_task1.has_submission_limit)
+
+    @override_config(MAX_SUBMISSIONS=0)
+    def test_custom_submission_limit_when_zero(self):
+        self.assertIsNone(self.published_task1.max_submissions)
+        self.assertEqual(self.published_task1.submission_limit, 0)
+        self.assertTrue(self.published_task1.has_submission_limit)
+
+    def test_submission_limit_local_override(self):
+        task1 = Task.objects.create(
+            title='Test Task 1',
+            system_name='TestTask1',
+            category=self.category1,
+            pubdate=timezone.now(),
+            max_submissions=0
+        )
+        task2 = Task.objects.create(
+            title='Test Task 2',
+            system_name='TestTask2',
+            category=self.category1,
+            pubdate=timezone.now(),
+            max_submissions=10
+        )
+        self.assertEqual(task1.submission_limit, 0)
+        self.assertTrue(task1.has_submission_limit)
+        self.assertEqual(task2.submission_limit, 10)
+        self.assertTrue(task2.has_submission_limit)
+
+    @override_config(MAX_SUBMISSIONS=10)
+    def test_custom_submission_limit_local_override(self):
+        task = Task.objects.create(
+            title='Test Task',
+            system_name='TestTask',
+            category=self.category1,
+            pubdate=timezone.now(),
+            max_submissions=15
+        )
+        self.assertEqual(task.submission_limit, 15)
+        self.assertTrue(task.has_submission_limit)
 
 
 class TaskCategoryTests(SimpleAccountsData, TaskData, TestCase):
