da874fd57621099fb51a4c61880dded1794494ab
Martin Morgenstern <mail@mmorgenstern.net>
2020-08-12 12:11:33 +0200

    Add live config for the syntax check service URL

--- inloop/solutions/constance.py
+++ inloop/solutions/constance.py
@@ -1,14 +1,20 @@
-from collections import OrderedDict
-
 from django.utils.safestring import mark_safe
 
-config = OrderedDict()
-config['ALLOWED_FILENAME_EXTENSIONS'] = ('.java', mark_safe(
-    'Filename extensions to be accepted in solution upload forms '
-    'such as <code>.java, .c, .cpp, .h, .hpp</code>. '
-    'Separate multiple filename extensions by commas. '
-    'Surrounding whitespace will be stripped and '
-    'filename extensions will be treated case insensitive.'
-))
+config = {
+    'ALLOWED_FILENAME_EXTENSIONS': ('.java', mark_safe(
+        'Filename extensions to be accepted in solution upload forms '
+        'such as <code>.java, .c, .cpp, .h, .hpp</code>. '
+        'Separate multiple filename extensions by commas. '
+        'Surrounding whitespace will be stripped and '
+        'filename extensions will be treated case insensitive.'
+    )),
+    'SYNTAX_CHECK_ENDPOINT': ('', (
+        'The URL of the syntax check endpoint. If empty, syntax checks '
+        'will be disabled globally.'
+    )),
+}
 
-fieldsets = {'General settings': ['ALLOWED_FILENAME_EXTENSIONS']}
+fieldsets = {
+    'General settings': ['ALLOWED_FILENAME_EXTENSIONS'],
+    'Syntax check': ['SYNTAX_CHECK_ENDPOINT']
+}
