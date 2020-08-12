ecabfee2d2a05e81681aa8b25fda774c11a50535
Martin Morgenstern <mail@mmorgenstern.net>
2020-08-12 11:05:19 +0200

    Move solution upload tests to the right place

--- tests/solutions/test_upload_view.py
+++ tests/solutions/test_upload_view.py
@@ -3,6 +3,7 @@ import shutil
 from tempfile import mkdtemp
 from unittest.mock import patch
 
+from django.contrib.messages import get_messages
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, override_settings, tag
 from django.urls import reverse
@@ -12,7 +13,6 @@ from inloop.solutions.models import Solution, SolutionFile
 from inloop.tasks.models import Category, Task
 from inloop.testrunner.models import TestOutput, TestResult
 
-from tests.integration.tools import MessageTestCase
 from tests.solutions.mixins import SolutionsData
 
 FIBONACCI = """
@@ -41,6 +41,29 @@ public class Fibonacci {
 """
 
 TEST_MEDIA_ROOT = mkdtemp()
+
+
+class MessageTestCase(TestCase):
+    """Supply basic assertions for the Django Messages framework."""
+
+    class Levels:
+        DEBUG = 10
+        INFO = 20
+        SUCCESS = 25
+        WARNING = 30
+        ERROR = 40
+
+    def assertResponseContainsMessage(self, text, level, response, msg=None):
+        messages = list(get_messages(response.wsgi_request))
+        for message in messages:
+            if message.message == text and message.level == level:
+                return
+        msg = self._formatMessage(
+            msg,
+            'The given response did not contain a message '
+            'with text "{}" and level "{}"'.format(text, level)
+        )
+        raise self.failureException(msg)
 
 
 @tag('slow')
