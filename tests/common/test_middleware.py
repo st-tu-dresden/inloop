ee82588511b4484049cf049cb386deb0c24a5fdf
Martin Morgenstern <martin.morgenstern1@tu-dresden.de>
2021-01-20 11:20:04 +0100

    Fix bogus test of SetRemoteAddrFromForwardedFor

--- tests/common/test_middleware.py
+++ tests/common/test_middleware.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.test import RequestFactory, SimpleTestCase, TestCase, modify_settings
 
 from inloop.common.middleware import SetRemoteAddrFromForwardedFor
@@ -7,17 +8,21 @@ class SetRemoteAddrFromForwardedForTestCase(SimpleTestCase):
     FACTORY = RequestFactory()
 
     def setUp(self):
-        self.middleware = SetRemoteAddrFromForwardedFor(lambda r: r)
+        self.middleware = SetRemoteAddrFromForwardedFor(lambda r: HttpResponse())
 
-    def test_x_forwarded_for(self):
-        request = self.middleware(self.FACTORY.get("/", HTTP_X_FORWARDED_FOR="1.1.1.1"))
+    def test_x_forwarded_for1(self):
+        request = self.FACTORY.get("/", HTTP_X_FORWARDED_FOR="1.1.1.1")
+        self.middleware(request)
         self.assertEqual(request.META["REMOTE_ADDR"], "1.1.1.1")
 
-        request = self.middleware(self.FACTORY.get("/", HTTP_X_FORWARDED_FOR="3.3.3.3, 4.4.4.4"))
+    def test_x_forwarded_for2(self):
+        request = self.FACTORY.get("/", HTTP_X_FORWARDED_FOR="3.3.3.3, 4.4.4.4")
+        self.middleware(request)
         self.assertEqual(request.META["REMOTE_ADDR"], "3.3.3.3")
 
     def test_no_x_forwarded_for(self):
-        request = self.middleware(self.FACTORY.get("/", REMOTE_ADDR="1.2.3.4"))
+        request = self.FACTORY.get("/", REMOTE_ADDR="1.2.3.4")
+        self.middleware(request)
         self.assertEqual(request.META["REMOTE_ADDR"], "1.2.3.4")
 
 
