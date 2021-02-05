3ea8a83c01bbf92863a85061e9e6c26179b8d80e
Martin Morgenstern <martin.morgenstern1@tu-dresden.de>
2021-02-05 11:16:06 +0100

    Always list tasks in non-empty categories
    Fixes: #422

--- tests/tasks/test_views.py
+++ tests/tasks/test_views.py
@@ -2,26 +2,49 @@ from django.contrib.auth.models import Group
 from django.test import TestCase
 from django.urls import reverse
 
+from inloop.tasks.models import Category
+
 from tests.accounts.mixins import SimpleAccountsData
 from tests.tasks.mixins import TaskData
 
 
-class IndexViewTest(SimpleAccountsData, TaskData, TestCase):
+class CategoryViewTest(SimpleAccountsData, TaskData, TestCase):
     def setUp(self):
-        url = reverse("tasks:category", kwargs={"slug": self.category1.slug})
         self.assertTrue(self.client.login(username="bob", password="secret"))
-        self.response = self.client.get(url, follow=True)
 
-    def test_task_visibility(self):
-        self.assertContains(self.response, self.published_task1.title)
-        self.assertContains(self.response, self.published_task2.title)
-        self.assertContains(self.response, self.unpublished_task1.title)
-        self.assertContains(self.response, self.unpublished_task2.title)
+    def get_url(self, slug):
+        return reverse("tasks:category", kwargs={"slug": slug})
 
-    def test_task_styling(self):
-        html = str(self.response.rendered_content)
-        self.assertTrue('<tr style="color: lightgrey;"' in html)
-        self.assertTrue("<tr>" in html)
+    def test_view_empty_category(self):
+        category = Category.objects.create(name="Empty category")
+        response = self.client.get(self.get_url(category.slug))
+        self.assertContains(response, "Nothing to do here")
+
+    def test_view_all_unpublished_category(self):
+        category = Category.objects.create(name="Unpublished tasks")
+        self.unpublished_task1.category = category
+        self.unpublished_task1.save()
+        response = self.client.get(self.get_url(category.slug))
+        self.assertContains(response, self.unpublished_task1.title)
+        self.assertContains(response, '<tr class="tasks-unpublished">')
+        self.assertNotContains(response, "Nothing to do here")
+
+    def test_view_all_published_category(self):
+        category = Category.objects.create(name="Published tasks")
+        self.published_task1.category = category
+        self.published_task1.save()
+        response = self.client.get(self.get_url(category.slug))
+        self.assertContains(response, self.published_task1.title)
+        self.assertNotContains(response, "Nothing to do here")
+        self.assertNotContains(response, '<tr class="tasks-unpublished">')
+
+    def test_view_category(self):
+        response = self.client.get(self.get_url(self.category1.slug))
+        self.assertContains(response, self.published_task1.title)
+        self.assertContains(response, self.published_task2.title)
+        self.assertContains(response, self.unpublished_task1.title)
+        self.assertContains(response, self.unpublished_task2.title)
+        self.assertNotContains(response, "Nothing to do here")
 
 
 class TaskDetailViewTest(SimpleAccountsData, TaskData, TestCase):
